ba37e2b95644a5fb862a622045eaa1c98635eca9
Benjamin Weißenfels <b.pixeldrama@gmail.com>
2012-04-12 21:58:23 +0200

    added skeleton for lexer project ii

---
+++ bii/lexergenerator/src/main/java/lexergen/Lexer.java
@@ -0,0 +1,13 @@
+package lexergen;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lexer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

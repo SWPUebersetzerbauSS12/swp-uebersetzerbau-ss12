4d88ab6d232c5d60106a4f59885e234f14246281
Daniel Rotar <daniel.rotar@fu-berlin.de>
2012-04-22 14:33:18 +0200

    (1) Format updated (Eclipse: Source -> Format) to get a uniform format. (2) Umlaut's updated to UTF-8 format.

--- bii/lexergenerator/src/main/java/regextodfaconverter/fsm/FiniteStateMachine.java
+++ bii/lexergenerator/src/main/java/regextodfaconverter/fsm/FiniteStateMachine.java
@@ -8,16 +8,15 @@ import regextodfaconverter.fsm.excpetions.NullStateException;
 import regextodfaconverter.fsm.excpetions.StateNotReachableException;
 import regextodfaconverter.fsm.excpetions.TransitionAlreadyExistsException;
 
-
 /**
  * Stellt einen endlichen Automaten (finite state machine, kurz FSM) dar.
  * 
  * @author Daniel Rotar
  * 
  * @param <TransitionConditionType>
- *          Der Typ der Bedingung f�r einen Zustand�bergang.
+ *            Der Typ der Bedingung für einen Zustandsübergang.
  * @param <StatePayloadType>
- *          Der Typ des Inhalts der Zust�nde.
+ *            Der Typ des Inhalts der Zustände.
  */
 public class FiniteStateMachine<TransitionConditionType extends Comparable<TransitionConditionType>, StatePayloadType> {
 
@@ -30,11 +29,10 @@ public class FiniteStateMachine<TransitionConditionType extends Comparable<Trans
 	 */
 	private State<TransitionConditionType, StatePayloadType> _currentState;
 	/**
-	 * Die HashMap die alle Zust�nde dieses endlichen Automaten, geordnet nach
-	 * ihren eindetige ID enth�lt.
+	 * Die HashMap die alle Zustände dieses endlichen Automaten, geordnet nach
+	 * ihren eindetige ID enthält.
 	 */
 	private HashMap<UUID, State<TransitionConditionType, StatePayloadType>> _states;
-
 
 	/**
 	 * Gibt der Startzustand dieses endlichen Automatens.
@@ -45,10 +43,9 @@ public class FiniteStateMachine<TransitionConditionType extends Comparable<Trans
 		return _initialState;
 	}
 
-
 	/**
-	 * Gibt den aktuelle Zustand, in dem sich dieser endlichen Automatens befindet
-	 * zur�ck.
+	 * Gibt den aktuelle Zustand, in dem sich dieser endlichen Automatens
+	 * befindet zurück.
 	 * 
 	 * @return Der aktuelle Zustand, in dem sich dieser endlichen Automatens
 	 *         befindet.
@@ -57,106 +54,102 @@ public class FiniteStateMachine<TransitionConditionType extends Comparable<Trans
 		return _currentState;
 	}
 
-
 	/**
 	 * Setzt den aktuelle Zustand, in dem sich dieser endlichen Automatens
 	 * befindet fest.
 	 * 
 	 * @param state
-	 *          Der neue Zustand, in dem sich dieser endlichen Automatens befinden
-	 *          soll.
+	 *            Der neue Zustand, in dem sich dieser endlichen Automatens
+	 *            befinden soll.
 	 * @throws NullStateException
-	 *           Wenn null als Parameter f�r den neuen Zustand �bergeben wird.
+	 *             Wenn null als Parameter für den neuen Zustand übergeben wird.
 	 * @throws StateNotReachableException
-	 *           Wenn der Zustand nicht erreichbar oder Teil des endlichen
-	 *           Automats ist.
+	 *             Wenn der Zustand nicht erreichbar oder Teil des endlichen
+	 *             Automats ist.
 	 */
 	public void setCurrentState(
 			State<TransitionConditionType, StatePayloadType> state)
 			throws NullStateException, StateNotReachableException {
-		if ( state == null)
+		if (state == null)
 			throw new NullStateException();
-		if ( !containsStateWithUUID( state.getUUID()))
+		if (!containsStateWithUUID(state.getUUID()))
 			throw new StateNotReachableException();
 		_currentState = state;
 	}
 
-
 	/**
-	 * Gibt den Zustand zur�ck, der die angegebene eindeutige UUID hat.
+	 * Gibt den Zustand zurück, der die angegebene eindeutige UUID hat.
 	 * 
-	 * @return Die eindeutige ID des Zustands, der zur�ckgegeben werden soll. Gibt
-	 *         es keinen Zustand, der die angegebene ID enth�lt wird null
-	 *         zur�ckgegeben.
+	 * @return Die eindeutige ID des Zustands, der zurückgegeben werden soll.
+	 *         Gibt es keinen Zustand, der die angegebene ID enthält wird null
+	 *         zurückgegeben.
 	 */
 	public State<TransitionConditionType, StatePayloadType> getStateByUUID(
 			UUID uuid) {
-		if ( _states.containsKey( uuid)) {
-			return _states.get( uuid);
+		if (_states.containsKey(uuid)) {
+			return _states.get(uuid);
 		} else {
 			return null;
 		}
 	}
-
 
 	/**
 	 * Gibt an ob der endliche Automat einen Zustand mit der angegebenen
 	 * eindeutigen UUID beinhaltet.
 	 * 
 	 * @param uuid
-	 *          Die UUID nach der in diesem endliche Automaten gesucht werden
-	 *          soll.
+	 *            Die UUID nach der in diesem endliche Automaten gesucht werden
+	 *            soll.
 	 * @return true, wenn die UUID in dem endliche Automat vorhanden ist, sonst
 	 *         false.
 	 */
-	public boolean containsStateWithUUID( UUID uuid) {
-		return _states.containsKey( uuid);
+	public boolean containsStateWithUUID(UUID uuid) {
+		return _states.containsKey(uuid);
 	}
 
-
 	/**
-	 * Gibt an, ob es sich bei diesem endlichen Automaten um einen deterministisch
-	 * endlichen Automaten (deterministic finite automaton, kurz DFA) handelt.
+	 * Gibt an, ob es sich bei diesem endlichen Automaten um einen
+	 * deterministisch endlichen Automaten (deterministic finite automaton, kurz
+	 * DFA) handelt.
 	 * 
 	 * @return true, wenn es sich um einen deterministisch endlichen Automaten
 	 *         (deterministic finite automaton, kurz DFA) handelt, sonst false.
 	 */
 	public boolean isDeterministic() {
-		for ( State<TransitionConditionType, StatePayloadType> state : _states
+		for (State<TransitionConditionType, StatePayloadType> state : _states
 				.values()) {
 			HashSet<TransitionConditionType> conditions = new HashSet<TransitionConditionType>();
-			for ( Transition<TransitionConditionType, StatePayloadType> transition : state
+			for (Transition<TransitionConditionType, StatePayloadType> transition : state
 					.getTransitions()) {
-				if ( transition.getCondition() == null)
-					return false; // Epsilon-�bergang vorhanden
-				if ( !conditions.add( transition.getCondition()))
-					return false; // �bergangsbedingung mehrfach vorhanden.
+				if (transition.getCondition() == null)
+					return false; // Epsilon-Übergang vorhanden
+				if (!conditions.add(transition.getCondition()))
+					return false; // Übergangsbedingung mehrfach vorhanden.
 			}
 		}
 
 		return true;
 	}
 
-
 	/**
-	 * Macht eine Zustands�nderung mit der angebenene Bedingung.
+	 * Macht eine Zustandsänderung mit der angebenene Bedingung.
 	 * 
 	 * @param condition
-	 *          Die Bedingung f�r die Zustand�nderung.
-	 * @return Der Zustand, der durch diese Zustand�nderung erreicht worden ist.
-	 *         Ist keine Zustand�nderung m�glich wird null ausgegeben und der
-	 *         aktuelle Zustand bleibt unver�ndert.
-	 * @remarks (1) null-�berg�nge bzw. Epislon-�berg�nge werden nicht
-	 *          ber�cksichtigt, k�nnen aber explizit, wie normale �berg�nge durch
-	 *          die Angabe von null als �bergangsbedinung durchgef�hrt werden. (2)
-	 *          Gibt es mehr als einen passenden �bergang wird Standardm��ig der
-	 *          erste passende �bergang ausgew�hlt.
+	 *            Die Bedingung für die Zustandsänderung.
+	 * @return Der Zustand, der durch diese Zustandsänderung erreicht worden
+	 *         ist. Ist keine Zustandsänderung möglich wird null ausgegeben und
+	 *         der aktuelle Zustand bleibt unverändert.
+	 * @remarks (1) null-Übergänge bzw. Epislon-Übergänge werden nicht
+	 *          berücksichtigt, können aber explizit, wie normale Übergänge
+	 *          durch die Angabe von null als Übergangsbedinung durchgeführt
+	 *          werden. (2) Gibt es mehr als einen passenden Übergang wird
+	 *          standardmäßig der erste passende Übergang ausgewählt.
 	 */
 	public State<TransitionConditionType, StatePayloadType> changeState(
 			TransitionConditionType condition) {
-		for ( Transition<TransitionConditionType, StatePayloadType> transition : getCurrentState()
+		for (Transition<TransitionConditionType, StatePayloadType> transition : getCurrentState()
 				.getTransitions()) {
-			if ( transition.getCondition().equals( condition)) {
+			if (transition.getCondition().equals(condition)) {
 				State<TransitionConditionType, StatePayloadType> state = transition
 						.getState();
 				_currentState = state;
@@ -167,114 +160,111 @@ public class FiniteStateMachine<TransitionConditionType extends Comparable<Trans
 		return null;
 	}
 
-
 	/**
-	 * Gibt an, ob eine Zustand�nderung mit der angegebenen Bedingung m�glich ist.
+	 * Gibt an, ob eine Zustandsänderung mit der angegebenen Bedingung möglich
+	 * ist.
 	 * 
 	 * @param condition
-	 *          Die Bedingung f�r die Zustand�nderung.
-	 * @return true, wenn eine Zustand�nderung m�glich ist, sonst false.
-	 * @remarks null-�berg�nge bzw. Epislon-�berg�nge werden nicht ber�cksichtigt,
-	 *          k�nnen aber explizit, wie normale �berg�nge durch die Angabe von
-	 *          null als �bergangsbedinung �berpr�ft werden.
+	 *            Die Bedingung für die Zustandsänderung.
+	 * @return true, wenn eine Zustandsänderung möglich ist, sonst false.
+	 * @remarks null-Übergänge bzw. Epislon-Übergänge werden nicht
+	 *          berücksichtigt, können aber explizit, wie normale Übergänge
+	 *          durch die Angabe von null als Übergangsbedinung überprüft
+	 *          werden.
 	 */
-	public boolean canChangeState( TransitionConditionType condition) {
-		for ( Transition<TransitionConditionType, StatePayloadType> transition : getCurrentState()
+	public boolean canChangeState(TransitionConditionType condition) {
+		for (Transition<TransitionConditionType, StatePayloadType> transition : getCurrentState()
 				.getTransitions()) {
-			if ( transition.getCondition().equals( condition))
+			if (transition.getCondition().equals(condition))
 				return true;
 		}
 
 		return false;
 	}
 
-
 	/**
-	 * Setzt den aktuellen Zustand auf den Startzustand zur�ck.
+	 * Setzt den aktuellen Zustand auf den Startzustand zurück.
 	 */
 	public void resetToInitialState() {
 		_currentState = _initialState;
 	}
 
-
 	/**
-	 * F�gt einen neuen �bergang mit angegebener Bedingung von einem
+	 * Fügt einen neuen Übergang mit angegebener Bedingung von einem
 	 * Ausgangszustand in einen Zielzustand ein.
 	 * 
 	 * @param sourceState
-	 *          Der Ausgangszustand, von dem ein neuer �bergang erstellt werden
-	 *          soll.
+	 *            Der Ausgangszustand, von dem ein neuer Übergang erstellt
+	 *            werden soll.
 	 * @param destinationState
-	 *          Der Zielzustand, in dem der neue �bergang zeigen soll.
+	 *            Der Zielzustand, in dem der neue Übergang zeigen soll.
 	 * @param condition
-	 *          Die Bedingung f�r den Zustand�bergang (null f�r einen
-	 *          Epsilon-�bergang).
+	 *            Die Bedingung für den Zustandsübergang (null für einen
+	 *            Epsilon-Übergang).
 	 * @throws NullStateException
-	 *           Wenn null als Parameter f�r den Ausgangszustand oder Zielzustand
-	 *           �bergeben wird.
+	 *             Wenn null als Parameter für den Ausgangszustand oder
+	 *             Zielzustand übergeben wird.
 	 * @throws StateNotReachableException
-	 *           Wenn der Ausgangszustand nicht erreichbar oder Teil des endlichen
-	 *           Automats ist.
+	 *             Wenn der Ausgangszustand nicht erreichbar oder Teil des
+	 *             endlichen Automats ist.
 	 * @throws TransitionAlreadyExistsException
-	 *           Wenn der �bergang bereits vorhanden ist.
+	 *             Wenn der Übergang bereits vorhanden ist.
 	 */
 	public void addTransition(
 			State<TransitionConditionType, StatePayloadType> sourceState,
 			State<TransitionConditionType, StatePayloadType> destinationState,
 			TransitionConditionType condition) throws NullStateException,
 			StateNotReachableException, TransitionAlreadyExistsException {
-		if ( sourceState == null || destinationState == null)
+		if (sourceState == null || destinationState == null)
 			throw new NullStateException();
-		if ( !containsStateWithUUID( sourceState.getUUID()))
+		if (!containsStateWithUUID(sourceState.getUUID()))
 			throw new StateNotReachableException();
 
-		if ( !containsStateWithUUID( destinationState.getUUID())) {
-			_states.put( destinationState.getUUID(), destinationState);
+		if (!containsStateWithUUID(destinationState.getUUID())) {
+			_states.put(destinationState.getUUID(), destinationState);
 		}
-		sourceState.addState( condition, destinationState);
+		sourceState.addState(condition, destinationState);
 	}
 
-
 	/**
-	 * F�gt einen neuen �bergang mit angegebener Bedingung von aktuellen Zustand
+	 * Fügt einen neuen Übergang mit angegebener Bedingung von aktuellen Zustand
 	 * in einen Zielzustand ein.
 	 * 
 	 * @param destinationState
-	 *          Der Zielzustand, in dem der neue �bergang zeigen soll.
+	 *            Der Zielzustand, in dem der neue Übergang zeigen soll.
 	 * @param condition
-	 *          Die Bedingung f�r den Zustand�bergang (null f�r einen
-	 *          Epsilon-�bergang).
+	 *            Die Bedingung für den Zustandsübergang (null für einen
+	 *            Epsilon-Übergang).
 	 * @throws NullStateException
-	 *           Wenn null als Parameter f�r den Zielzustand �bergeben wird.
+	 *             Wenn null als Parameter für den Zielzustand übergeben wird.
 	 * @throws TransitionAlreadyExistsException
-	 *           Wenn der �bergang bereits vorhanden ist.
+	 *             Wenn der Übergang bereits vorhanden ist.
 	 */
 	public void addTransition(
 			State<TransitionConditionType, StatePayloadType> destinationState,
 			TransitionConditionType condition) throws NullStateException,
 			TransitionAlreadyExistsException {
-		if ( destinationState == null)
+		if (destinationState == null)
 			throw new NullStateException();
 		try {
-			addTransition( _currentState, destinationState, condition);
-		} catch ( StateNotReachableException e) {
+			addTransition(_currentState, destinationState, condition);
+		} catch (StateNotReachableException e) {
 			// Dieser Fall kann niemals eintreten!
 			e.printStackTrace();
 		}
 	}
 
-
 	/**
 	 * Erstellt ein neues FiniteStateMachine Objekt. Dabei wird direkt ein
-	 * Startzustand f�r diesen endlichen Automaten erstellt und als aktuellen
+	 * Startzustand für diesen endlichen Automaten erstellt und als aktuellen
 	 * Zustand gesetzt.
 	 */
 	public FiniteStateMachine() {
 		State<TransitionConditionType, StatePayloadType> state = new State<TransitionConditionType, StatePayloadType>();
-		state.setType( StateType.INITIAL);
+		state.setType(StateType.INITIAL);
 		_initialState = state;
 		_currentState = state;
 		_states = new HashMap<UUID, State<TransitionConditionType, StatePayloadType>>();
-		_states.put( state.getUUID(), state);
+		_states.put(state.getUUID(), state);
 	}
 }
--- bii/lexergenerator/src/main/java/regextodfaconverter/fsm/StatePayload.java
+++ bii/lexergenerator/src/main/java/regextodfaconverter/fsm/StatePayload.java
@@ -2,20 +2,18 @@ package regextodfaconverter.fsm;
 
 import tokenmatcher.TokenType;
 
-
 public class StatePayload implements tokenmatcher.StatePayload {
 
 	private TokenType tokenType;
-	
+
 	private int backsteps = 0;
-	
-  
-	public StatePayload( TokenType tokenType, int backsteps) {
+
+	public StatePayload(TokenType tokenType, int backsteps) {
 		super();
 		this.tokenType = tokenType;
 		this.backsteps = backsteps;
 	}
-	
+
 	public TokenType getTokenType() {
 		return tokenType;
 	}
@@ -23,6 +21,5 @@ public class StatePayload implements tokenmatcher.StatePayload {
 	public int getBacksteps() {
 		return backsteps;
 	}
-	
 
 }

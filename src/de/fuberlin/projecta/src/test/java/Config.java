bb1025b1073dabaf082067ea4216ed33e5d4f88a
Christian Cikryt <c.cikryt@googlemail.com>
2012-04-22 12:44:50 +0200

    adjusted path to test file

--- src/de/fuberlin/projecta/src/test/java/Config.java
+++ src/de/fuberlin/projecta/src/test/java/Config.java
@@ -1,3 +1,3 @@
 public class Config {
-	public final static String TEST_DATA_FOLDER = "../resources/";
+	public final static String TEST_DATA_FOLDER = System.getProperty("user.dir") + "/target/test-classes/";
 }
--- src/de/fuberlin/projecta/src/test/java/LexerTest.java
+++ src/de/fuberlin/projecta/src/test/java/LexerTest.java
@@ -1,26 +1,25 @@
-import java.io.File;
-
 import lexer.ILexer;
 import lexer.Lexer;
 import lexer.SyntaxErrorException;
 import lexer.Token;
-
 import org.junit.Test;
+
+import java.io.File;
 
 public class LexerTest {
 
 	@Test
 	public void test() {
-		final String path = Config.TEST_DATA_FOLDER + "/LexerTestFile1.txt";
+		final String path = Config.TEST_DATA_FOLDER + "LexerTestFile1.txt";
 
 		File sourceFile = new File(path);
-		assert(sourceFile.exists());
-		assert(sourceFile.canRead());
+		assert (sourceFile.exists());
+		assert (sourceFile.canRead());
 
 		ILexer lex = new Lexer(path);
 		Token t;
 		try {
-			while((t = lex.getNextToken()) != null){
+			while ((t = lex.getNextToken()) != null) {
 				System.out.println(t);
 			}
 		} catch (SyntaxErrorException e) {

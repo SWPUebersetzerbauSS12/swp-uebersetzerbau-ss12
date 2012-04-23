05ac3da826af6f4f20fb768904640c9fad1d4b08
Kevin Funk <kevin.funk@fu-berlin.de>
2012-04-23 19:20:19 +0200

    Add StringCharStream + tests

---
+++ src/de/fuberlin/projecta/src/main/java/lexer/io/StringCharStream.java
@@ -0,0 +1,41 @@
+package lexer.io;
+
+import lombok.Getter;
+
+public class StringCharStream implements ICharStream {
+
+	private String data;
+
+	@Getter
+	private int offset;
+
+	public StringCharStream(String data) {
+		this.data = data;
+		this.offset = 0;
+	}
+
+	@Override
+	public String getNextChars(int numberOfChars) {
+		final int count = Math.min(numberOfChars, data.length());
+		return data.substring(0, count);
+	}
+
+	@Override
+	public int consumeChars(int numberOfChars) {
+		final int count = Math.min(numberOfChars, data.length());
+		data = data.substring(count, data.length());
+		this.offset += count;
+		return count;
+	}
+
+	@Override
+	public void resetOffset() {
+		this.offset = 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return data.length() == 0;
+	}
+
+}

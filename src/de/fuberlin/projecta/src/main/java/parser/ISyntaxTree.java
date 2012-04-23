8dba4afec082a6ed9a62dbb157978582342a0da2
Christian Cikryt <c.cikryt@googlemail.com>
2012-04-23 14:47:31 +0200

    removed "removeTree" method

--- src/de/fuberlin/projecta/src/main/java/parser/ISyntaxTree.java
+++ src/de/fuberlin/projecta/src/main/java/parser/ISyntaxTree.java
@@ -3,15 +3,12 @@ package parser;
 /**
  * Using composite pattern as tree data-structure. Leafs are indicated by a
  * childCount of 0.
- * 
+ *
  * @author Michael Wittig
- * 
  */
 public interface ISyntaxTree {
 
 	public void addTree(ISyntaxTree tree);
-
-	public void removeTree(int i);
 
 	public String getName();
 
@@ -27,18 +24,15 @@ public interface ISyntaxTree {
 	public ISyntaxTree getChildren(int i);
 
 	/**
-	 * 
 	 * @return The desired attribute. Null if none is found.
 	 */
 	public Attribute getAttribute(String name);
 
 	/**
 	 * Changes the value of the attribute
-	 * 
-	 * @param name
-	 *            the attribute's name
-	 * @param value
-	 *            the value to change the attribute to.
+	 *
+	 * @param name  the attribute's name
+	 * @param value the value to change the attribute to.
 	 * @return True if change was successful, False otherwise.
 	 */
 	public boolean setAttribute(String name, String value);
@@ -46,9 +40,8 @@ public interface ISyntaxTree {
 	/**
 	 * Adds a new attribute to the nodes attribute collection, if this name is
 	 * not already inserted.
-	 * 
-	 * @param name
-	 *            the attribute's name to insert.
+	 *
+	 * @param name the attribute's name to insert.
 	 * @return True if add was successful, False otherwise.
 	 */
 	public boolean addAttribute(String name);

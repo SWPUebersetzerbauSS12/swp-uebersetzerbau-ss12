4f38ef0af18db644dd106f4e37afa611830db21d
Daniel Rotar <daniel.rotar@fu-berlin.de>
2012-04-24 09:36:15 +0200

    Added missing comments.

--- bii/lexergenerator/src/main/java/regextodfaconverter/ConvertExecption.java
+++ bii/lexergenerator/src/main/java/regextodfaconverter/ConvertExecption.java
@@ -1,14 +1,30 @@
 package regextodfaconverter;
 
-
+/**
+ * Stellt einen Fehler dar, der beim Konvertieren eines regulären Ausdrucks in einen DFA vorkommt.
+ * @author Johannes Dahlke
+ *
+ */
 public class ConvertExecption extends Exception {
 	
 	
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = -2213213933640594695L;
+
+
+	/**
+	 * Erstellt ein neues ConvertExecption Objekt.
+	 */
 	public ConvertExecption() {
 		super();
 	}
 	
-
+	/**
+	 * Erstellt ein neues ConvertExecption Objekt.
+	 * @param message Die genaue Fehlerbeschreibung.
+	 */
 	public ConvertExecption( String message) {
 		super( message);
 	}

f55e95e9fec1635a350df980a3455a7b0f016b6b
PascalCase <jdbiz@web.de>
2012-04-26 08:50:06 +0200

    add a settings class for setting constants

---
+++ bii/lexergenerator/src/main/java/lexergen/Settings.java
@@ -0,0 +1,10 @@
+package lexergen;
+
+import tokenmatcher.errorhandler.ErrorCorrector.CorrectionMode;
+
+
+public class Settings {
+
+	
+	  public static final CorrectionMode ERROR_CORRECTION_MODE = CorrectionMode.PANIC_MODE;
+}

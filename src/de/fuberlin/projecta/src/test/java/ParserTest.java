ff54da9629b4016ad237c9e1a3e40a397dcb3e76
Michael Wittig <michael.wittig@fu-berlin.de>
2012-04-27 13:47:05 +0200

    added parser test

---
+++ src/de/fuberlin/projecta/src/test/java/ParserTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import lexer.Lexer;
+import lexer.SyntaxErrorException;
+import lexer.io.FileCharStream;
+
+import org.junit.Test;
+
+import parser.Parser;
+import parser.ParserException;
+
+
+public class ParserTest {
+
+	@Test
+	public void testReadSourceFile() throws SyntaxErrorException {
+		final String path = Config.TEST_DATA_FOLDER + "LexerTestFile1.txt";
+
+		File sourceFile = new File(path);
+		assertTrue(sourceFile.exists());
+		assertTrue(sourceFile.canRead());
+
+		Lexer lexer = new Lexer(new FileCharStream(path));
+		Parser parser = new Parser(lexer);
+		try {
+			parser.parse();
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+	}
+}

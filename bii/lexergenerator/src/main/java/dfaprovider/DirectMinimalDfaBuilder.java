a38bb03fe5ccb5c153c8210217ce2221f6450f1e
Daniel Rotar <daniel.rotar@fu-berlin.de>
2012-04-29 16:52:05 +0200

    DfaBuilder implemented.

--- bii/lexergenerator/src/main/java/dfaprovider/DirectMinimalDfaBuilder.java
+++ bii/lexergenerator/src/main/java/dfaprovider/DirectMinimalDfaBuilder.java
@@ -6,12 +6,13 @@ import regextodfaconverter.MinimalDfa;
 import regextodfaconverter.fsm.StatePayload;
 
 /**
- * Stellt einen MinimalDFA-Builder dar.
+ * Stellt einen MinimalDFA-Builder dar, der den DFA über den direkten Weg von
+ * Regex zu DFA erstellt.
  * 
- * @author Daniel Rotar
+ * @author ?
  * 
  */
-public interface MinimalDfaBuilder {
+public class DirectMinimalDfaBuilder {
 
 	/**
 	 * Erstellt den minimalen DFA für die angegebenen regulären Definitionen und
@@ -25,5 +26,8 @@ public interface MinimalDfaBuilder {
 	 *             Wenn ein Fehler beim Erstellen des DFA's auftritt.
 	 */
 	public MinimalDfa<Character, StatePayload> buildMinimalDfa(
-			File regularDefinitionFile) throws MinimalDfaBuilderException;
+			File regularDefinitionFile) throws MinimalDfaBuilderException {
+		// TODO: buildMinimalDfa implementieren.
+		return null;
+	}
 }

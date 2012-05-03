75eb14d9cf46a45fbb4e04b5a1962a92d9ae0b05
stefan <stlenz@inf.fu-berlin.de>
2012-05-03 18:06:08 +0200

    Token erst mal für die Ausdrucksgrammatik des Drachenbuchs implementiert

---
+++ src/de/fuberlin/projectci/extern/IToken.java
@@ -0,0 +1,61 @@
+package de.fuberlin.projectci.extern;
+
+public interface IToken {
+
+	/**
+	 * TokenType enum für Ausdrucksgrammatik
+	 * TODO TokenType als Enum macht für einen Parsergenarator wenig Sinn (es sei denn man will für jede Grammatik die TokenTypes neu generieren)
+	 * TODO Muss noch mit den anderen Gruppen abgestimmt werden
+	 */
+	public enum TokenType {
+		OP_ADD("+"),  
+		OP_MUL ("*"),
+		ID ("id"),
+		LPAREN("("),
+		RPAREN(")"),
+		EOF("$");
+		
+		private final String terminalSymbol;
+		
+		private TokenType(String terminalSymbol) {
+			this.terminalSymbol=terminalSymbol;
+		}
+		
+		public String terminalSymbol(){
+			return this.terminalSymbol;
+		}
+		
+	}
+
+	/**
+	 * Get the type of this Token
+	 * 
+	 * @return Token type
+	 */
+	TokenType getType();
+
+	/**
+	 * Get the Token attribute value
+	 * 
+	 * E.g. for a Token of type REAL this can be "0.0"
+	 * 
+	 * @return Attribute value
+	 */
+	String getAttribute();
+
+	/**
+	 * Get the start offset of this Token's attribute
+	 * 
+	 * @note The position is relative to the beginning of the line
+	 * 
+	 * @return Start offset
+	 */
+	int getOffset();
+
+	/**
+	 * Get the line number of this Token's attribute
+	 * 
+	 * @return End offset
+	 */
+	int getLineNumber();
+}

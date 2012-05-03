59d1573bee0830f6c9056847bde486c2861b1386
David Knötel <david.knoetel@fu-berlin.de>
2012-05-03 14:46:35 +0200

    Changed LLVMBlock

--- src/de/fuberlin/optimierung/LLVMBlock.java
+++ src/de/fuberlin/optimierung/LLVMBlock.java
@@ -2,24 +2,21 @@ package de.fuberlin.optimierung;
 
 class LLVMBlock implements ILLVMBlock{
 	
-	LLVMCommand firstCommand;
-	LLVMCommand lastCommand;
-	String label;
-	
-	public LLVMBlock(){
-		commands = new LLVMCommand<List>();
-	}
-	
-	public LLVMBlock(String label){
-		this.label = label;
-		commands = new LLVMCommand<List>();
-	}
-	
-	public void addLabel(String label){
-		this.label = label;
-	}
+	LLVMCommand firstCommand = null;
+	LLVMCommand lastCommand = null;
+	String label = "";
 	
 	public void addCmdLine(String cmd){
+		
+		if(!this.firstCommand) {
+			// Fuege ersten Befehl ein
+			return;
+		}
+		// if(Command ist sprung) {
+		//		letzter Befehl
+		// }
+		
+		// Weder erster noch letzter Befehl des Blocks
 		LLVMCommand cmdLine = new LLVMCommand(cmd);
 		commands.add(cmdLine);
 	}

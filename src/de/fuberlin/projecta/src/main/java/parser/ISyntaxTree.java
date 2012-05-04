2788ee309b500125e8f94c6ab3aa166c42233672
Michael Wittig <michael.wittig@fu-berlin.de>
2012-05-04 17:15:03 +0200

    added run function for semantic analysis

--- src/de/fuberlin/projecta/src/main/java/parser/ISyntaxTree.java
+++ src/de/fuberlin/projecta/src/main/java/parser/ISyntaxTree.java
@@ -18,7 +18,8 @@ public interface ISyntaxTree {
 	public int getChildrenCount();
 
 	/**
-	 * @param i child index
+	 * @param i
+	 *            child index
 	 * @return the i'th child of current node, null if none is existing.
 	 */
 	public ISyntaxTree getChild(int i);
@@ -28,14 +29,15 @@ public interface ISyntaxTree {
 	 */
 	public IAttribute getAttribute(String name);
 
-
 	public List<ISyntaxTree> getChildrenByName(String name);
 
 	/**
 	 * Changes the value of the attribute
-	 *
-	 * @param name the attribute's name
-	 * @param value the value to change the attribute to.
+	 * 
+	 * @param name
+	 *            the attribute's name
+	 * @param value
+	 *            the value to change the attribute to.
 	 * @return True if change was successful, False otherwise.
 	 */
 	public boolean setAttribute(String name, String value);
@@ -43,9 +45,15 @@ public interface ISyntaxTree {
 	/**
 	 * Adds a new attribute to the nodes attribute collection, if this name is
 	 * not already inserted.
-	 *
-	 * @param name the attribute's name to insert.
+	 * 
+	 * @param name
+	 *            the attribute's name to insert.
 	 * @return True if add was successful, False otherwise.
 	 */
 	public boolean addAttribute(String name);
+
+	/**
+	 * Semantic rules !
+	 */
+	public void run();
 }

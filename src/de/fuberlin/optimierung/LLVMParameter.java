69500c29b6fcecf9bef3f417d7e42a6281d6dcb7
David Knötel <david.knoetel@fu-berlin.de>
2012-05-06 13:27:34 +0200

    Added LLVMParameter Setter and Getter.

--- src/de/fuberlin/optimierung/LLVMParameter.java
+++ src/de/fuberlin/optimierung/LLVMParameter.java
@@ -2,13 +2,37 @@ package de.fuberlin.optimierung;
 
 public class LLVMParameter {
 
-	private String typeString;
-	private LLVMParameterType type;
-	private String name;
+	private String typeString;		// Bsp: "i32"
+	private LLVMParameterType type;		// Bsp: REGISTER
+	private String name;	// Bsp: %i
 	
 	public LLVMParameter(String name, LLVMParameterType type, String typeString) {
 		this.typeString = typeString;
 		this.type = type;
+		this.name = name;
+	}
+
+	public String getTypeString() {
+		return typeString;
+	}
+
+	public void setTypeString(String typeString) {
+		this.typeString = typeString;
+	}
+
+	public LLVMParameterType getType() {
+		return type;
+	}
+
+	public void setType(LLVMParameterType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
 	

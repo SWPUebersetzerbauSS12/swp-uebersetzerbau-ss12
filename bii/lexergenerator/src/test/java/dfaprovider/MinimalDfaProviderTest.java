24061f02a813466de9b3dcb87dadf86ebafeea63
Daniel Rotar <daniel.rotar@fu-berlin.de>
2012-05-09 13:08:10 +0200

    Updatet the structure of the test dir (e.g. moved the test-*.fun files to a test-resource dir)

---
+++ bii/lexergenerator/src/test/java/dfaprovider/MinimalDfaProviderTest.java
@@ -0,0 +1,23 @@
+package dfaprovider;
+
+import lexergen.Settings;
+
+import org.junit.Test;
+
+/**
+ * Test-Klasse für die Regex-Klasse.
+ * @author Maximilian Schröder
+ *
+ */
+public class MinimalDfaProviderTest {
+
+	/**
+	 * Test of getMinimalDfa method, of class ReadTokDefinition.
+	 */
+	@Test
+	public void testGetMinimalDfa() throws Exception {
+		Settings.readSettings();
+		
+		//TODO @Max: JUnit Test schreiben.
+	}
+}
--- bii/lexergenerator/src/test/java/tokenmatcher/TokenizerTest.java
+++ bii/lexergenerator/src/test/java/tokenmatcher/TokenizerTest.java
@@ -1,9 +1,10 @@
-package lexergen;
-
-import static org.junit.Assert.*;
+package tokenmatcher;
 
 import java.util.ArrayList;
 
+import lexergen.Settings;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import regextodfaconverter.ConvertExecption;
@@ -15,17 +16,48 @@ import tokenmatcher.StatePayload;
 import tokenmatcher.Token;
 import tokenmatcher.TokenType;
 import tokenmatcher.Tokenizer;
-import bufferedreader.BufferedLexemeReader;
-import bufferedreader.LexemeReader;
+import tokenmatcher.errorhandler.ErrorCorrector.CorrectionMode;
+import bufferedreader.*;
 
-public class LexerTest {
+/**
+ * Test-Klasse für die Tokenizer-Klasse.
+ * @author  Johannes Dahlke
+ *
+ */
+public class TokenizerTest {
 
+	/**
+	 * Test of getNextToken method, of class Tokenizer.
+	 */
 	@Test
-	public void test() throws ConvertExecption, Exception {
+	public void testGetNextToken() throws ConvertExecption, Exception {
 		Settings.readSettings();
-		runTest();
-	}
+		Settings.setErrorCorrectionMode(CorrectionMode.PANIC_MODE); // Dieser Test ist nur im PANIC_MODE durchführbar.
+		
+		FiniteStateMachine<Character, StatePayload> fsm = generateRelopFSM();
+		fsm.union(generateCommentFSM());
+		NfaToDfaConverter<Character, StatePayload> nfaToDfaConverter = new NfaToDfaConverter<Character, StatePayload>();
+		fsm = nfaToDfaConverter.convertToDfa(fsm);
 
+		LexemeReader lexemeReader = new BufferedLexemeReader("src/test/resources/source/testrelop.fun");
+		//LexemeReader lexemeReader = new SimpleLexemeReader("src/test/resources/source/testrelop.fun");
+
+		Tokenizer tokenizer = new Tokenizer(lexemeReader,
+				new MinimalDfa<Character, StatePayload>(fsm));
+
+		Token currentToken;
+		String tokenString;
+		String[] tokensToFind = {"<OP, LE>", "<OP, LT>", "<OP, NE>", "<OP, LT>", "<OP, NE>", "<OP, LT>", "<OP, NE>", "<OP, LE>", "<OP, LT>", "<OP, LT>"};
+		int i = 0;
+		while (true) {
+			currentToken = tokenizer.getNextToken();
+			tokenString = "<" + currentToken.getType() + ", " + currentToken.getAttribute() + ">";
+			Assert.assertEquals(tokensToFind[i], tokenString);
+			System.out.println(tokenString);
+			i++;
+		}
+	}
+	
 	/**
 	 * Erstellt einen Automaten für Wörter, die gültige Zahlen darstellen.
 	 * 
@@ -206,29 +238,6 @@ public class LexerTest {
 		}
 
 		return fsm;
-	}
-
-	public static void runTest() throws ConvertExecption, Exception {
-
-		FiniteStateMachine<Character, StatePayload> fsm = generateRelopFSM();
-		// FiniteStateMachine<Character, StatePayload> fsm =
-		// generateCommentFSM();
-		fsm.union(generateCommentFSM());
-		NfaToDfaConverter<Character, StatePayload> nfaToDfaConverter = new NfaToDfaConverter<Character, StatePayload>();
-		fsm = nfaToDfaConverter.convertToDfa(fsm);
-
-		LexemeReader lexemeReader = new BufferedLexemeReader("testrelop.fun");// new
-																				// SimpleLexemeReader(
-																				// "testrelop.fun");
-		Tokenizer tokenizer = new Tokenizer(lexemeReader,
-				new MinimalDfa<Character, StatePayload>(fsm));
-
-		Token currentToken;
-		while (true) {
-			currentToken = tokenizer.getNextToken();
-			System.out.print(currentToken.getType());
-			System.out.println("  " + currentToken.getAttribute());
-		}
 	}
 
 }

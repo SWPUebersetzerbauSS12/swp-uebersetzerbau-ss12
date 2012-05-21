425c13b7b0b40498aaa2a7298a2f0da79494853d
stefan <stlenz@inf.fu-berlin.de>
2012-05-21 22:43:57 +0200

    Vergleichbarkeit mit Hilfe von EasyComparableObject sichergestellt. toString implementiert. NullPointerException in getAction und getGoto verhindert.

--- src/de/fuberlin/projectci/parseTable/ParseTable.java
+++ src/de/fuberlin/projectci/parseTable/ParseTable.java
@@ -2,7 +2,9 @@ package de.fuberlin.projectci.parseTable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
+import de.fuberlin.commons.util.EasyComparableObject;
 import de.fuberlin.projectci.grammar.NonTerminalSymbol;
 import de.fuberlin.projectci.grammar.TerminalSymbol;
 
@@ -10,21 +12,26 @@ import de.fuberlin.projectci.grammar.TerminalSymbol;
  * Repräsention der Action- und Goto-Tabellen.
  *
  */
-public class ParseTable {
+public class ParseTable extends EasyComparableObject{		
 	private Map<State, ActionTable> state2ActionTable = new HashMap<State, ParseTable.ActionTable>();
 	private Map<State, GotoTable> state2GotoTable = new HashMap<State, ParseTable.GotoTable>();
 	private State initialState;
+	
+	@Override
+	protected Object[] getSignificantFields() {
+		return new Object[]{initialState,state2ActionTable,state2GotoTable};
+	}
 	
 	public State getInitialState(){
 		return initialState;
 	}
 
-	public Action getAction(State s, TerminalSymbol nts) {
-		return state2ActionTable.get(s).getAction(nts);
+	public Action getAction(State s, TerminalSymbol ts) {
+		return getActionTableForState(s).getAction(ts);
 	}
 	 
-	public Goto getGoto(State s, NonTerminalSymbol ts) {
-		return state2GotoTable.get(s).getGoto(ts);
+	public Goto getGoto(State s, NonTerminalSymbol nts) {
+		return getGotoTableForState(s).getGoto(nts);
 	}
 	
 	public void setInitialState(State initialState){
@@ -45,21 +52,54 @@ public class ParseTable {
 		return state2GotoTable.get(s);
 	}
 	
-	public static class ActionTable{
+	public static class ActionTable extends EasyComparableObject{
+		private static Action ERROR_ACTION=new ErrorAction();
 		private Map<TerminalSymbol, Action> terminalSymbol2Action= new HashMap<TerminalSymbol, Action>();
+		
+		@Override
+		protected Object[] getSignificantFields() {
+			return new Object[]{terminalSymbol2Action, getClass()};
+		}
 		
 		public Action getAction(TerminalSymbol nts) {
 			Action a= terminalSymbol2Action.get(nts);
-			return a!=null?a:new ErrorAction();
+			return a!=null?a:ERROR_ACTION;
 		}
 		
 		public void setActionForTerminalSymbol(Action action, TerminalSymbol terminalSymbol){
 			terminalSymbol2Action.put(terminalSymbol, action);
 		}
+		
+		@Override
+		public String toString() {
+			StringBuffer strBuf=new StringBuffer();
+			Set<TerminalSymbol> terminalSymbols = terminalSymbol2Action.keySet();
+			strBuf.append("[");
+			int i=0;
+			for (TerminalSymbol aTerminalSymbol : terminalSymbols) {
+				if (i>0){
+					strBuf.append(", ");
+				}
+				strBuf.append(aTerminalSymbol);
+				strBuf.append(" → ");
+				strBuf.append(getAction(aTerminalSymbol));
+				i++;
+			}
+			strBuf.append("]");
+			return strBuf.toString();
+		}
+
+		
+		
 	}
 	
-	public static class GotoTable{
+	public static class GotoTable extends EasyComparableObject{
 		private Map<NonTerminalSymbol, Goto> nonTerminalSymbol2Goto= new HashMap<NonTerminalSymbol, Goto>();
+		
+		@Override
+		protected Object[] getSignificantFields() {
+			return new Object[]{nonTerminalSymbol2Goto, getClass()};
+		}
 		
 		public Goto getGoto(NonTerminalSymbol nonTerminalSymbol){
 			return nonTerminalSymbol2Goto.get(nonTerminalSymbol);
@@ -68,7 +108,44 @@ public class ParseTable {
 		public void setGotoForNonTerminalSymbol(Goto _goto, NonTerminalSymbol nonTerminalSymbol){
 			nonTerminalSymbol2Goto.put(nonTerminalSymbol, _goto);
 		}
+		
+		@Override
+		public String toString() {
+			StringBuffer strBuf=new StringBuffer();
+			Set<NonTerminalSymbol> nonTerminalSymbols = nonTerminalSymbol2Goto.keySet();
+			strBuf.append("[");
+			int i=0;
+			for (NonTerminalSymbol aNonTerminalSymbol : nonTerminalSymbols) {
+				if (i>0){
+					strBuf.append(", ");
+				}
+				strBuf.append(aNonTerminalSymbol);
+				strBuf.append(" → ");
+				strBuf.append(getGoto(aNonTerminalSymbol).getTargetState().getName());
+				i++;
+			}
+			strBuf.append("]");
+			return strBuf.toString();
+		}
 	}
+	
+	@Override
+	public String toString() {		
+		StringBuffer strBuf=new StringBuffer();
+		// Annahme: Es gibt keinen State zu dem es ein Goto aber keine Action gibt
+		Set<State> states = state2ActionTable.keySet();
+		for (State aState : states) {
+			strBuf.append(aState);
+			strBuf.append(": ");
+			strBuf.append(getActionTableForState(aState));
+			strBuf.append(" ");
+			strBuf.append(getGotoTableForState(aState));
+			strBuf.append("\n");
+		}
+		
+		return strBuf.toString();
+	}
+
 	
 }
  

0b108e3e32a4dd767fb23d2eaee1fadb4c6d125b
Patrick Schlott <patrick.schlott@fu-berlin.de>
2012-05-21 18:45:18 +0200

    Fixed a Bug in Leftfactorisation; Added Comments to Grammarreader; Leftfactorisation now keeps sorting of elements; Added Iteration of Eliminating left rekursions

--- src/de/fuberlin/projectcii/ParserGenerator/src/GrammarReader.java
+++ src/de/fuberlin/projectcii/ParserGenerator/src/GrammarReader.java
@@ -1,39 +1,84 @@
 import java.io.*;
 import java.util.*;
 
-/* 
- * GrammarReader reads a Grammar in BNF from a given file
+
+/**
+ * Reads a grammar from a given file and initialises it for better use.
+ * Before the Grammar is returned, direct-, and indirect
+ * left-rekursions are eliminated and left-factorisation is performed. 
+ * Elimination of left-rekursions is fixed at 4 Runs after which the Grammar
+ * is deemed not LL(1)-parsable.
  * 
- * Author: Patrick Schlott
- * 
+ * @author Patrick Schlott
  */
+
 public class GrammarReader {
 	
-	public Vector<Productions> grammar;
+	// intern representation of Grammar
+	private Vector<Productions> grammar;
+	// the Startsymbol
 	private String startSymbol;
 	/*
-	 * Reads the file from the given file in BNF
-	 * and Converts it to a Vector of Nonterminals
+	 
 	 * 
 	 * Input:()
 	 * Returnvalue: Vector<Nonterminals> containing the read grammar
 	 */
 	
+	
+	/**
+	 * Getter for the Startsymbol
+	 * 
+	 * @return The start symbol for the read Grammar
+	 * @author Patrick Schlott
+	 */
 	public String getStartSymbol(){
 		return startSymbol;
 	}
 	
+	/**
+	 * Reads a grammar from the given file in BNF
+	 * and converts it to a Hash-Map containing the Productions.
+	 * Before the grammar is returned, direct-, and indirect
+	 * left-rekursions are eliminated and left-factorisation is performed. 
+	 * elimination of left-rekursions is fixed at 4 runs after which the Grammar
+	 * is deemed not LL(1)-parsable.
+	 * 
+	 * @author Patrick Schlott
+	 * @param file The path to the file containing the grammar that shall be used.
+	 * @return The grammar as HashMap where the keys are the heads of the production
+	 * and the values are vectors containing the productions itself. Each production
+	 * is represented as a vector of terminal and nonterminal symbols
+	 */
 	public Map<String, Vector<Vector<String>>> createGrammar(String file){
+		// Read the file
 		Vector<Productions>grammar=ReadFile(file);
+		// Perform Leftfactorisation
 		grammar=combineLeftFactorization(grammar);
-		grammar=eliminateIndirectLeftRekursion(grammar);
-		grammar=eliminateDirectLeftRekursion(grammar);
+		// Eliminate indirect and direct leftrekursions
+		Boolean[] rekursive = {true};
+		int iteration = 0;
+		// stop at 4 Iterations, the fifth checks is the grammar is deemed unparsable
+		while (rekursive[0] && iteration < 5){
+			rekursive[0] = false;
+			grammar=eliminateIndirectLeftRekursion(grammar,rekursive);
+			grammar=eliminateDirectLeftRekursion(grammar);
+			iteration++;
+		}
 		this.grammar = grammar;
-		printGrammar();
+		// definie first Element in Vector as startsymbol
 		startSymbol = grammar.elementAt(0).getHead();
 		return buildGrammarMap(grammar);
 	}
 	
+	/**
+	 * Reads a grammar from the given file in BNF
+	 * and converts it to Vector of Productions.
+	 * 
+	 * @author Patrick Schlott
+	 * @param file The path to the file containing the grammar that shall be used.
+	 * @return The grammar as a Vector of Productions	 
+	 */
 	private Vector<Productions> ReadFile(String file){
 		
 		//Vector the nonterminals are saved to
@@ -65,46 +110,62 @@ public class GrammarReader {
 		    in.close();
 		} catch (IOException e) {
 		}
-		
-		
+
 		return grammar;
 	}
 	
+	/**
+	 * Performs left-factorisation on a given grammar.
+	 * 
+	 * @author Patrick Schlott
+	 * @param grammar Grammar to be left-factorised
+	 * @return The Modified grammar as a Vector of Productions	 
+	 */
 	private Vector<Productions> combineLeftFactorization(Vector<Productions> grammar) {
 		//Vector containing the modified grammar
 		Vector <Productions> grammarMod = new Vector <Productions>();
 
 		//Go through every Nonterminal in the grammar
 		for (Productions nonterminal:grammar){
-			
+			// Counter for naming the new productions
 			int counter=0;
 			
+			// Go through the Nonterminal as long as there is something to do
 			boolean goOn = true;
 			while (goOn){
 				Vector<Vector<String>> productions = nonterminal.productions;
+				
+				//Calculates which Factor is the longest
+				//Returns 0: size of longest Factor
+				//        1: smallest Productionnumber containing this faktor
 				
 				int[]factor = calculateLongestFactor(productions);
 				
 				int productionNr=factor[1];
 				int longest=factor[0];
 				
+				// check if a Factor exists
 				if (longest > 0){
+					// First Production containing the Factor
 					Vector<String> matchingProduction = nonterminal.productions.elementAt(productionNr);
-					String head=nonterminal.getHead();
+					// Initiate new Nonterminals
+					String head = nonterminal.getHead();
 					String head1 = head.substring(0, head.length()-1)+counter+">";
 					Productions nonTerminalOld = new Productions(head);
 					Productions nonTerminalNew = new Productions(head1);
+					// Add Factor + Head1 to Old Production
 					Vector<String> factorisation = new Vector<String>();
 					for (int j=0; j<longest;j++){
 						factorisation.add(productions.elementAt(productionNr).elementAt(j));
 					}
 					factorisation.add(head1);
 					nonTerminalOld.InsertProduction(factorisation);
-					
+					// Go through all Productions and check if they Containing the Factor
 					for (Vector<String> production:productions){
 						if (production.size() >= longest){
 							boolean matched = true;
 							int i = 0;
+							// check weather the Production contains the Factor
 							while (matched){
 								if (i<longest){
 									if (matchingProduction.elementAt(i).equals(production.elementAt(i))){
@@ -114,9 +175,12 @@ public class GrammarReader {
 								}
 								else matched = false;
 							}
+							// deside where to put the production depending on i and longest
 							if (i<longest){
+								// Productions is not part of the factorisation
 								nonTerminalOld.InsertProduction(production);
 							}
+							// Production contains the longest factor
 							else if (i==longest){
 								if (longest == production.size()){
 									Vector<String> epsilon = new Vector<String>();
@@ -124,12 +188,11 @@ public class GrammarReader {
 									nonTerminalNew.InsertProduction(epsilon);
 								}
 								else{
+									Vector<String> newProduction = new Vector<String>();
 									for (int k=i; k<production.size() ;k++){
-										Vector<String> newProduction = new Vector<String>();
-										newProduction.add(production.elementAt(k));
-										nonTerminalNew.InsertProduction(newProduction);
-										
+										newProduction.add(production.elementAt(k));										
 									}
+									nonTerminalNew.InsertProduction(newProduction);
 								}
 							}
 						}
@@ -137,11 +200,26 @@ public class GrammarReader {
 							nonTerminalOld.InsertProduction(production);
 						}
 					}
-					counter++;
+					// add new and old Production back in the modified grammar
 					grammarMod.remove(nonterminal);
 					nonterminal = nonTerminalOld;
 					grammarMod.add(nonTerminalOld);
 					grammarMod.add(nonTerminalNew);
+					// keep the sorting of productions intact
+					for (int i=0; i<counter;i++){
+						//int productNr = 0;
+						for (Productions p:grammarMod)
+						{
+							if (p.getHead().equals(head.substring(0, head.length()-1)+i+">")){
+								//Productions savedProduction = p;
+								grammarMod.remove(p);
+								grammarMod.add(p);
+								break;
+							}
+						
+						}
+					}
+					counter++;
 				}
 				else{
 					if(counter == 0){
@@ -149,25 +227,33 @@ public class GrammarReader {
 					}
 					goOn=false;
 				}
-				System.out.println(nonterminal.getHead()+" : "+longest+" : "+productionNr);
 			}
 		}
 		return grammarMod;
 	}
 	
-
+	/**
+	 * Calculates the longest Factor in the Production
+	 * 
+	 * @author Patrick Schlott
+	 * @param productions The Productions of the nonterminal
+	 * @return An array containing [0]: size of the longest factor
+	 * 							   [1]: first production containing the factor
+	 */
 
 	private int[] calculateLongestFactor(Vector<Vector<String>> productions) {
-		int productionNr=0;
-		int longest=0;
-		
+		int productionNr=0; // Returnvalue 1
+		int longest=0; // Returnvalue 0
+		// iterate through every production
 		int productionCnt = productions.size();
 		for (int i=0; i<productionCnt;i++){
 			Vector<String> production = productions.elementAt(i);
+			// compare symbol-vice with other productions
 			for(int j=i+1;j<productionCnt;j++){
 				Vector<String> productionMatch = productions.elementAt(j);
 				boolean matched = true;
 				int k=0;
+				// calculate longest match
 				while (matched){
 					if (production.size() > k && productionMatch.size() > k){
 						if (production.elementAt(k).equals(productionMatch.elementAt(k))){
@@ -177,6 +263,7 @@ public class GrammarReader {
 					}
 					else matched = false;
 				}
+				// check if new Factor is longer than the old
 				if (longest < k){
 					longest = k;
 					productionNr = i;
@@ -187,26 +274,39 @@ public class GrammarReader {
 		return factor ;
 	}
 	
-	private Vector <Productions> eliminateIndirectLeftRekursion(Vector <Productions> grammar) {
+	
+	/**
+	 * Eliminates indirect left-factorisation on a given grammar.
+	 * 
+	 * @author Patrick Schlott
+	 * @param grammar Grammar to be examined
+	 * @param rekursive Call by Reference bool, indicating weather the grammar has been modified
+	 * @return The Modified grammar as a Vector of Productions	 
+	 */
+	private Vector <Productions> eliminateIndirectLeftRekursion(Vector <Productions> grammar, Boolean[] rekursive) {
 
 		Vector <Productions> grammarMod = new Vector <Productions>();
 		int NonTerminalCounter = grammar.size();
+		//add first grammar-element as it can not be indirect left rekursive
 		grammarMod.add(grammar.firstElement());
-		
+		//go through all grammar-elements
 		for (int i = 1; i < NonTerminalCounter;i++){
 			Productions nonTerminalCurrent = grammar.elementAt(i);
+			// initalise bitmap indicating which Productions are leftrekursive
 			boolean[] bitmap = new boolean[nonTerminalCurrent.productions.size()];
 			for (int y=0; y<nonTerminalCurrent.productions.size();y++){
 				bitmap[y] = false;
 			}
+			// init new nonTerminal
 			Productions nonTerminalNew = new Productions(nonTerminalCurrent.getHead());
-			boolean rekursive = false;
+			rekursive[0] = false;
+			// check all nonterminals after i if they have i as first Element
 			for (int j = 0; j<i; j++){
 				Productions nonTerminalChecked = grammarMod.elementAt(j);
 				int x = 0;
 				for (Vector<String> production:nonTerminalCurrent.productions){
 					if (production.firstElement().equals(nonTerminalChecked.getHead())){
-						rekursive = true;
+						rekursive[0] = true;
 						bitmap[x] = true;
 						for (Vector<String> productionChecked:nonTerminalChecked.productions){
 							Vector<String> newProduction = new Vector<String>();
@@ -222,7 +322,7 @@ public class GrammarReader {
 					x++;
 				}
 			}
-			if (rekursive){
+			if (rekursive[0]){
 				for (int y=0; y<nonTerminalCurrent.productions.size();y++){
 					if (!bitmap[y]){
 						nonTerminalNew.InsertProduction(nonTerminalCurrent.productions.elementAt(y));
@@ -236,14 +336,15 @@ public class GrammarReader {
 		}
 		return grammarMod;
 	}
-
-	/*
-	 * Eliminates direct leftrekursions be introducing new Nonterminal
-	 * <"N"1>
+	
+	/**
+	 * Eliminates direct leftrekursions by introducing new Nonterminal <"N"$>
 	 * 
-	 * Input: Void
-	 * Return: Void
+	 * @author Patrick Schlott
+	 * @param grammar Grammar to be examined
+	 * @return The Modified grammar as a Vector of Productions	 
 	 */
+
 	private Vector <Productions> eliminateDirectLeftRekursion(Vector <Productions> grammar){
 		//Vector containing the modified grammar
 		Vector <Productions> grammarMod = new Vector <Productions>();
@@ -319,6 +420,16 @@ public class GrammarReader {
 		return grammarMod;
 	}
 	
+	
+	/**
+	 * Transforms the internal Vector to an equivalent Hashmap.
+	 * The Hashmap has the form: Map<String, Vector<Vector<String>>>
+	 * with the head of the productions as keys.
+	 * 
+	 * @author Ying Wei
+	 * @param grammar Grammar to be transformed
+	 * @return The transformed Grammar	 
+	 */
 	private Map<String, Vector<Vector<String>>> buildGrammarMap(Vector<Productions> grammar) {
 		Map<String, Vector<Vector<String>>> gMap = new LinkedHashMap<String, Vector<Vector<String>>>();
 		for (Productions p : grammar) {
@@ -327,8 +438,22 @@ public class GrammarReader {
 		return gMap;
 	}
 	
-	//Visual debug-Function printing the grammar 
+	/**
+	 * Visual debugging Function printing the global Grammar.
+	 * 
+	 * @author Patrick Schlott
+	 */
+	@SuppressWarnings("unused")
 	private void printGrammar(){
+		
+		
+		/* To use this function in the main methode use:
+		 * this.grammar = grammar;
+		 * System.out.print(<Text>);
+		 * printGrammar();
+		*/
+		
+		
 		
 		for (int i = 0; i< grammar.size() ; i++){
 			Productions nonterminal = grammar.elementAt(i);

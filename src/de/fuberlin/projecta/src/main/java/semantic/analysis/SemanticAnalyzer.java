38539e3dc72696f39e68a20daeff8bede68d3c4f
Michael Wittig <michael.wittig@fu-berlin.de>
2012-05-21 09:36:58 +0200

    wrote some code to traverse parseTree in dfs to create ast

--- src/de/fuberlin/projecta/src/main/java/semantic/analysis/SemanticAnalyzer.java
+++ src/de/fuberlin/projecta/src/main/java/semantic/analysis/SemanticAnalyzer.java
@@ -1,20 +1,235 @@
 package semantic.analysis;
 
+import lexer.IToken.TokenType;
+import lombok.Getter;
 import parser.ISyntaxTree;
+import parser.ITree.DefaultAttribute;
+import parser.NonTerminal;
+import parser.Symbol.Reserved;
+import semantic.analysis.AstNodes.Array;
+import semantic.analysis.AstNodes.BasicType;
+import semantic.analysis.AstNodes.Block;
+import semantic.analysis.AstNodes.Break;
+import semantic.analysis.AstNodes.Declaration;
+import semantic.analysis.AstNodes.Do;
+import semantic.analysis.AstNodes.FuncDef;
+import semantic.analysis.AstNodes.If;
+import semantic.analysis.AstNodes.IfElse;
+import semantic.analysis.AstNodes.IntLiteral;
+import semantic.analysis.AstNodes.Params;
+import semantic.analysis.AstNodes.Print;
+import semantic.analysis.AstNodes.Program;
+import semantic.analysis.AstNodes.Return;
+import semantic.analysis.AstNodes.Statement;
+import semantic.analysis.AstNodes.Type;
+import semantic.analysis.AstNodes.While;
 
 public class SemanticAnalyzer {
 
-	private ISyntaxTree tree;
+	private ISyntaxTree parseTree;
 	private SymbolTableStack tables;
+	@Getter
+	private ISyntaxTree AST;
 
 	public SemanticAnalyzer(ISyntaxTree tree) {
-		this.tree = tree;
+		this.parseTree = tree;
 		tables = new SymbolTableStack();
 	}
-	
+
 	public void analyze() throws SemanticException {
-		parseTreeForSemanticActions(tree);
+		toAST(parseTree);
 		parseTreeForRemoval();
+		parseTreeForSemanticActions(AST);
+	}
+
+	public void toAST(ISyntaxTree tree) {
+		toAST(tree, null);
+	}
+
+	/**
+	 * Traverses through the parseTree in depth-first-search and adds new nodes
+	 * to the given insertNode. Only l-attributed nodes must be passed through
+	 * node-attributes!
+	 * 
+	 * The idea is that one node knows where it should get added to.
+	 * 
+	 * @param tree
+	 *            current parseTree-node (with symbol)
+	 * @param insertNode
+	 *            syntaxTree-Node, in which new nodes get added
+	 */
+	public void toAST(ISyntaxTree tree, ISyntaxTree insertNode) {
+
+		if (tree.getSymbol().isNonTerminal()) {
+			NonTerminal nonT = tree.getSymbol().asNonTerminal();
+
+			switch (nonT) {
+			case program:
+				AST = new Program();
+				toAST(tree.getChild(0), AST);
+				return;
+			case funcs:
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), insertNode);
+				}
+				return;
+			case func:
+				FuncDef func = new FuncDef();
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), func);
+				}
+				insertNode.addChild(func);
+				return;
+			case type:
+				Type type = new Type();
+
+				if (tree.getChild(0).getSymbol().asNonTerminal() == NonTerminal.basic) {
+					if (tree.getChild(1).getChildrenCount() != 0) { // this is
+																	// type_ and
+																	// it must
+																	// exist!
+						Array array = new Array();
+
+						// the basic node gets added to this temporary node
+						// and is passed to the new array
+						ISyntaxTree tmp = new Program(); // it doesn't matter
+															// which node to
+															// take as long as
+															// it is a treenode.
+						toAST(tree.getChild(0), tmp);
+
+						array.addAttribute("type");
+						boolean success = array.setAttribute("type",
+								tmp.getChild(0)); // this is already the
+													// BasicType node
+						assert (success);
+						for (int i = 0; i < tree.getChild(1).getChildrenCount(); i++) {
+
+						}
+					} else { // type_ is empty so only add basic type to type
+						for (int i = 0; i < tree.getChildrenCount(); i++) {
+							toAST(tree.getChild(i), type);
+						}
+						insertNode.addChild(type);
+					}
+
+				}
+
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), type);
+				}
+				insertNode.addChild(type);
+				return;
+			case optparams:
+				Params params = new Params();
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), params);
+				}
+				insertNode.addChild(params);
+				return;
+			case block:
+				Block block = new Block();
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), block);
+				}
+				if (block.getChildrenCount() != 0) {
+					insertNode.addChild(block);
+				} // else func_ -> ;
+				return;
+			case decl:
+				Declaration decl = new Declaration();
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), decl);
+				}
+				insertNode.addChild(decl);
+				return;
+			case stmt:
+				Statement stmt = null;
+				ISyntaxTree firstChild = tree.getChild(0);
+				if (firstChild.getSymbol().asTerminal() == TokenType.IF) {
+					ISyntaxTree stmt_ = tree.getChild(5); //
+					if (stmt_.getChildrenCount() == 0)
+						stmt = new If();
+					else
+						stmt = new IfElse();
+				} else if (firstChild.getSymbol().asTerminal() == TokenType.WHILE) {
+					stmt = new While();
+				} else if (firstChild.getSymbol().asTerminal() == TokenType.DO) {
+					stmt = new Do();
+				} else if (firstChild.getSymbol().asTerminal() == TokenType.BREAK) {
+					stmt = new Break();
+				} else if (firstChild.getSymbol().asTerminal() == TokenType.RETURN) {
+					stmt = new Return();
+				} else if (firstChild.getSymbol().asTerminal() == TokenType.PRINT) {
+					stmt = new Print();
+				} else if (firstChild.getSymbol().asNonTerminal() == NonTerminal.block) {
+					stmt = new Block();
+				}
+
+				if (stmt != null) {
+					for (int i = 0; i < tree.getChildrenCount(); i++) {
+						toAST(tree.getChild(i), stmt);
+					}
+					insertNode.addChild(stmt);
+				} else {
+					throw new SemanticException(
+							"stmt could'nt be set to any node in semantic analyzer");
+				}
+				return;
+			case loc:
+				// basic uses default
+				// stmt_ uses default
+				// stmt__ uses default
+				// stmts uses the default
+				// decls uses the default
+				// params uses the default
+				// params_ uses the default
+				// func_ uses the default
+			default:
+				// nothing to do here just pass it through
+				for (int i = 0; i < tree.getChildrenCount(); i++) {
+					toAST(tree.getChild(i), insertNode);
+				}
+				return;
+			}
+
+		} else if (tree.getSymbol().isTerminal()) {
+			TokenType t = tree.getSymbol().asTerminal();
+			switch (t) {
+			case BOOL_TYPE:
+				insertNode.addChild(new BasicType(TokenType.BOOL_TYPE));
+				return;
+			case INT_TYPE:
+				insertNode.addChild(new BasicType(TokenType.INT_TYPE));
+				return;
+			case REAL_TYPE:
+				insertNode.addChild(new BasicType(TokenType.REAL_TYPE));
+				return;
+			case STRING_TYPE:
+				insertNode.addChild(new BasicType(TokenType.STRING_TYPE));
+				return;
+			case INT_LITERAL:
+				// TODO: this must be type-checked if it's working.
+				insertNode.addChild(new IntLiteral((Integer) tree
+						.getAttribute(DefaultAttribute.TokenValue.name())));
+				return;
+			}
+		} else if (tree.getSymbol().isReservedTerminal()) {
+			Reserved res = tree.getSymbol().asReservedTerminal();
+			switch (res) {
+			case EPSILON:
+				if (tree.getChildrenCount() == 1) {
+					toAST(tree.getChild(0));
+				} else {
+					// this should never occur
+					throw new SemanticException(
+							"Epsilon in other position than head?");
+				}
+			case SP:
+				// this should never occur
+				throw new SemanticException("Stack pointer in parsetree?");
+			}
+		}
 	}
 
 	/**

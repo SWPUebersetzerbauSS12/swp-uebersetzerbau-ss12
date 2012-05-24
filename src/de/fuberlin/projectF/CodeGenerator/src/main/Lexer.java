843ba61ef7fea89685af100c7c2a9cded5675b72
Henry <henrydettmer@gmail.com>
2012-05-24 17:14:30 +0200

    Constructor mit Argument hinzugefügt.

--- src/de/fuberlin/projectF/CodeGenerator/src/main/Lexer.java
+++ src/de/fuberlin/projectF/CodeGenerator/src/main/Lexer.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.io.*;
 
 import main.model.Token;
@@ -8,19 +9,18 @@ import main.model.TokenType;
  * 
  * @author Peter Hirschfeld
  * 
- * Dies ist ein Lexer für einen LLVM Code.
- * Grobe Funktionsweise:
- * 		Der LLVM Code wird zeilenweise durchgeparst und es wird pro geparste Zeile ein Token erzeugt
- * 		und zurückgegeben.
+ *         Dies ist ein Lexer für einen LLVM Code. Grobe Funktionsweise: Der
+ *         LLVM Code wird zeilenweise durchgeparst und es wird pro geparste
+ *         Zeile ein Token erzeugt und zurückgegeben.
  * 
- * 		Es gibt 3 wesentliche Funktionen:
- * 			open: öffnet die LLVM-Code Datei
- * 			close: schließt die LLVM-Code Datei
- * 			getNextToken: liesst eine Zeile der Eingabe Datei und parst diese dann auf relevante Informationen
- * 			Diese Informationen werden dann in einem Token gespeichert. Der Token wird dann zurückgegeben.
- *
- * Ist aber noch nicht fertig!!!!!! 
- *
+ *         Es gibt 3 wesentliche Funktionen: open: öffnet die LLVM-Code Datei
+ *         close: schließt die LLVM-Code Datei getNextToken: liesst eine Zeile
+ *         der Eingabe Datei und parst diese dann auf relevante Informationen
+ *         Diese Informationen werden dann in einem Token gespeichert. Der Token
+ *         wird dann zurückgegeben.
+ * 
+ *         Ist aber noch nicht fertig!!!!!!
+ * 
  */
 
 public class Lexer {
@@ -28,51 +28,53 @@ public class Lexer {
 	FileInputStream fstream;
 	DataInputStream in;
 	BufferedReader br;
-	
-	public Lexer() {}
-	
-	//öffnen der Datei
+
+	public Lexer(String filename) {
+		this.open(filename);
+	}
+
+	// öffnen der Datei
 	public int open(String filename) {
-		try{
+		try {
 			fstream = new FileInputStream(filename);
 			in = new DataInputStream(fstream);
 			br = new BufferedReader(new InputStreamReader(in));
 
-			}catch (Exception e){
-			  System.err.println("Error: " + e.getMessage());
-			}
-		return 0;
-	}
-	
-	public int close() {
-		//schließen der Datei
-		try{
-		 in.close();
-		}catch (Exception e){
+		} catch (Exception e) {
 			System.err.println("Error: " + e.getMessage());
 		}
 		return 0;
 	}
-	
+
+	public int close() {
+		// schließen der Datei
+		try {
+			in.close();
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+		return 0;
+	}
+
 	public Token getNextToken() {
 		String strLine;
 		String[] splitLine;
-		
+
 		try {
-			//Einlesen der nächsten Zeile
-			while((strLine = br.readLine()) != null) {
-				
+			// Einlesen der nächsten Zeile
+			while ((strLine = br.readLine()) != null) {
+
 				splitLine = splitInformation(strLine);
-				if(splitLine.length == 0)
+				if (splitLine.length == 0)
 					continue;
-				
+
 				return fillToken(splitLine);
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.err.println("Error: " + e.getMessage());
-		
+
 		}
-		
+
 		Token endToken = new Token();
 		endToken.setType(TokenType.EOF);
 		return endToken;
@@ -80,10 +82,10 @@ public class Lexer {
 
 	private String[] splitInformation(String line) {
 		line = line.trim();
-		
+
 		String[] tmpSplitLine;
 		String[] splitLine;
-		
+
 		line = line.replace("(", " (");
 		line = line.replace(")", ") ");
 		line = line.replace("[", " [");
@@ -92,62 +94,71 @@ public class Lexer {
 		line = line.replace("}", "} ");
 		line = line.replace(",", " , ");
 		line = line.replace(":", " : ");
-		
+
 		int p1 = line.lastIndexOf('(');
 		int p2 = line.indexOf(')', p1);
-		line = replaceBetween(line,p1,p2,' ',(char)1);
-		
+		line = replaceBetween(line, p1, p2, ' ', (char) 1);
+
 		p1 = line.lastIndexOf('[');
 		p2 = line.indexOf(']', p1);
-		line = replaceBetween(line,p1,p2,' ',(char)1);
-		
+		line = replaceBetween(line, p1, p2, ' ', (char) 1);
+
 		p1 = line.lastIndexOf('{');
 		p2 = line.indexOf('}', p1);
-		line = replaceBetween(line,p1,p2,' ',(char)1);
-		
+		line = replaceBetween(line, p1, p2, ' ', (char) 1);
+
 		p1 = line.indexOf('"');
 		p2 = line.indexOf('"', p1 + 1);
-		line = replaceBetween(line,p1,p2,' ',(char)1);
-		
+		line = replaceBetween(line, p1, p2, ' ', (char) 1);
+
 		System.out.println(line);
-		
+
 		tmpSplitLine = line.split(" ");
-		
+
 		int count = 0;
-		for(int i = 0; i < tmpSplitLine.length; i++) {
-			if(tmpSplitLine[i].isEmpty()) {}
-			else if(tmpSplitLine[i].contentEquals(",")) {}
-			else if(tmpSplitLine[i].contentEquals("nounwind")) {}
-			else if(tmpSplitLine[i].contentEquals("nsw")) {}
-			else if(tmpSplitLine[i].contentEquals("align")) {break;}
-			
-			else{count++;}
+		for (int i = 0; i < tmpSplitLine.length; i++) {
+			if (tmpSplitLine[i].isEmpty()) {
+			} else if (tmpSplitLine[i].contentEquals(",")) {
+			} else if (tmpSplitLine[i].contentEquals("nounwind")) {
+			} else if (tmpSplitLine[i].contentEquals("nsw")) {
+			} else if (tmpSplitLine[i].contentEquals("align")) {
+				break;
+			}
+
+			else {
+				count++;
+			}
 		}
-		
+
 		splitLine = new String[count];
-		
+
 		count = 0;
-		for(int i = 0; i < tmpSplitLine.length; i++) {
-			if(tmpSplitLine[i].isEmpty()) {}
-			else if(tmpSplitLine[i].contentEquals(",")) {}
-			else if(tmpSplitLine[i].contentEquals("nounwind")) {}
-			else if(tmpSplitLine[i].contentEquals("nsw")) {}
-			else if(tmpSplitLine[i].contentEquals("align")) {break;}
-			
-			else{splitLine[count++] = new String(tmpSplitLine[i]);}
+		for (int i = 0; i < tmpSplitLine.length; i++) {
+			if (tmpSplitLine[i].isEmpty()) {
+			} else if (tmpSplitLine[i].contentEquals(",")) {
+			} else if (tmpSplitLine[i].contentEquals("nounwind")) {
+			} else if (tmpSplitLine[i].contentEquals("nsw")) {
+			} else if (tmpSplitLine[i].contentEquals("align")) {
+				break;
+			}
+
+			else {
+				splitLine[count++] = new String(tmpSplitLine[i]);
+			}
 		}
-		
+
 		return splitLine;
 	}
-	
-	private String replaceBetween(String line, int startpoint, int endpoint, char oldChar, char newChar) {
 
-		if(startpoint == -1)
+	private String replaceBetween(String line, int startpoint, int endpoint,
+			char oldChar, char newChar) {
+
+		if (startpoint == -1)
 			return line;
-		if(endpoint == -1)
+		if (endpoint == -1)
 			return line;
 
-		String tmpLine1,tmpLine2;
+		String tmpLine1, tmpLine2;
 		tmpLine1 = line.substring(startpoint, endpoint + 1);
 		tmpLine2 = line.substring(endpoint + 1);
 		line = line.substring(0, startpoint);
@@ -155,177 +166,176 @@ public class Lexer {
 		line = line.concat(tmpLine2);
 		return line;
 	}
-	
 
 	private Token fillToken(String[] line) {
 		Token newToken = new Token();
-		
-		//Definitionen (bei Methodendeklarationen)
-		if(line[0].contentEquals("define")) {
+
+		// Definitionen (bei Methodendeklarationen)
+		if (line[0].contentEquals("define")) {
 			newToken.setType(TokenType.Definition);
 			newToken.setTarget(line[2]);
 			newToken.setTypeTarget(line[1]);
-			fillParameter(newToken, line[3].replace((char)1, ' '));
+			fillParameter(newToken, line[3].replace((char) 1, ' '));
 		}
-		
+
 		// Wertzuweisungen
-		else if(line[0].contentEquals("store")) {
+		else if (line[0].contentEquals("store")) {
 			newToken.setType(TokenType.Assignment);
 			newToken.setTarget(line[4]);
 			newToken.setTypeTarget(line[3]);
 			newToken.setOp1(line[2]);
 			newToken.setTypeOp1(line[1]);
 		}
-		
-		else if(line[0].contentEquals("call")) {
+
+		else if (line[0].contentEquals("call")) {
 			newToken.setType(TokenType.Call);
 			newToken.setOp1(line[2]);
 			newToken.setTypeTarget(line[1]);
-			fillParameter(newToken, line[3].replace((char)1, ' '));
+			fillParameter(newToken, line[3].replace((char) 1, ' '));
 		}
-		
-		else if(line[0].contentEquals("br")) {
+
+		else if (line[0].contentEquals("br")) {
 			newToken.setType(TokenType.Branch);
 			newToken.setTarget(line[2]);
 		}
-		
-		//Return anweisungen
-		else if(line[0].contentEquals("ret")) {
+
+		// Return anweisungen
+		else if (line[0].contentEquals("ret")) {
 			newToken.setType(TokenType.Return);
 			newToken.setTypeOp1(line[1]);
-			if(line.length > 2) {
+			if (line.length > 2) {
 				newToken.setOp1(line[2]);
 			}
 		}
-		
-		//Ende einer Definition
-		else if(line[0].contentEquals("}")) {
+
+		// Ende einer Definition
+		else if (line[0].contentEquals("}")) {
 			newToken.setType(TokenType.DefinitionEnd);
 		}
-		
-		else if(line[1].contentEquals("<label>")) {
+
+		else if (line[1].contentEquals("<label>")) {
 			newToken.setType(TokenType.Label);
 			newToken.setTarget(line[3]);
 		}
-		
-		else if(line[1].contentEquals("=")) {
-			
-			//Typ-Definition (STRUCT, RECORD)
-			if(line[2].contentEquals("type")) {
+
+		else if (line[1].contentEquals("=")) {
+
+			// Typ-Definition (STRUCT, RECORD)
+			if (line[2].contentEquals("type")) {
 				newToken.setType(TokenType.TypeDefinition);
 				newToken.setTarget(line[0]);
-				fillParameter(newToken, line[3].replace((char)1,' '));
+				fillParameter(newToken, line[3].replace((char) 1, ' '));
 			}
-			
-			else if(line[0].startsWith("@.str")) {
+
+			else if (line[0].startsWith("@.str")) {
 				newToken.setType(TokenType.String);
 				newToken.setTarget(line[0]);
-				newToken.setTypeTarget(line[5].replace((char)1, ' '));
-				newToken.setOp1(line[6].substring(1).replace((char)1, ' '));
+				newToken.setTypeTarget(line[5].replace((char) 1, ' '));
+				newToken.setOp1(line[6].substring(1).replace((char) 1, ' '));
 			}
-	
-			//Additionen
-			else if(line[2].contentEquals("add") || line[2].contentEquals("fadd")) {
-				//Type: ADDITION
+
+			// Additionen
+			else if (line[2].contentEquals("add")
+					|| line[2].contentEquals("fadd")) {
+				// Type: ADDITION
 				newToken.setType(TokenType.Addition);
 				newToken.setTarget(line[0]);
 				newToken.setTypeTarget(line[3]);
 				newToken.setOp1(line[4]);
 				newToken.setOp2(line[5]);
 			}
-			//Subtraktionen
-			else if(line[2].contentEquals("sub")) {
+			// Subtraktionen
+			else if (line[2].contentEquals("sub")) {
 				newToken.setType(TokenType.Subtraction);
 				newToken.setTarget(line[0]);
 				newToken.setTypeTarget(line[3]);
 				newToken.setOp1(line[4]);
 				newToken.setOp2(line[5]);
 			}
-			//Wert aus Speicher lesen
-			else if(line[2].contentEquals("load")) {
+			// Wert aus Speicher lesen
+			else if (line[2].contentEquals("load")) {
 				newToken.setType(TokenType.Load);
 				newToken.setTarget(line[0]);
 				newToken.setOp1(line[4]);
 				newToken.setTypeOp1(line[3]);
 			}
-			
-			//Speicher Allocierungen
-			else if( line[2].contentEquals("alloca") ) {
+
+			// Speicher Allocierungen
+			else if (line[2].contentEquals("alloca")) {
 				newToken.setType(TokenType.Allocation);
 				newToken.setTarget(line[0]);
-				newToken.setTypeTarget(line[3].replace((char)1, ' '));
-			}
-			else if(line[2].contentEquals("call")) {
+				newToken.setTypeTarget(line[3].replace((char) 1, ' '));
+			} else if (line[2].contentEquals("call")) {
 				newToken.setType(TokenType.Call);
 				newToken.setTarget(line[0]);
 				newToken.setTypeTarget(line[3]);
 				newToken.setOp1(line[4]);
-				fillParameter(newToken, line[5].replace((char)1, ' '));
+				fillParameter(newToken, line[5].replace((char) 1, ' '));
 			}
-			
-			else if(line[2].contentEquals("icmp")) {
-				if(line[3].contentEquals("slt"))
+
+			else if (line[2].contentEquals("icmp")) {
+				if (line[3].contentEquals("slt"))
 					newToken.setType(TokenType.CompareLower);
-				else if(line[3].contentEquals("sgt"))
+				else if (line[3].contentEquals("sgt"))
 					newToken.setType(TokenType.CompareGreater);
-				else if(line[3].contentEquals("sle"))
+				else if (line[3].contentEquals("sle"))
 					newToken.setType(TokenType.CompareLowerEqual);
-				else if(line[3].contentEquals("sge"))	
+				else if (line[3].contentEquals("sge"))
 					newToken.setType(TokenType.CompareGreaterEqual);
-				else if(line[3].contentEquals("eq"))	
+				else if (line[3].contentEquals("eq"))
 					newToken.setType(TokenType.CompareEqual);
-				else if(line[3].contentEquals("ne"))	
+				else if (line[3].contentEquals("ne"))
 					newToken.setType(TokenType.CompareNotEqual);
 				else
 					newToken.setType(TokenType.Undefined);
-				
+
 				newToken.setTarget(line[0]);
 				newToken.setOp1(line[5]);
 				newToken.setTypeOp1(line[4]);
 				newToken.setOp2(line[6]);
-				
-			}
-			else
+
+			} else
 				newToken.setType(TokenType.Undefined);
-		}
-		else
+		} else
 			newToken.setType(TokenType.Undefined);
-		
+
 		return newToken;
 	}
 
 	private void fillParameter(Token newToken, String strLine) {
-		
+
 		int p1 = strLine.indexOf('(');
 		String pair;
 		String[] split;
-		
-		if (p1==-1)
-			p1=strLine.indexOf('{');
+
+		if (p1 == -1)
+			p1 = strLine.indexOf('{');
 		int p2 = p1;
 
-		//TODO: unschön aber funktioniert erstmal
-		while(true) {
+		// TODO: unschön aber funktioniert erstmal
+		while (true) {
 			do {
 				p2++;
-			}while(strLine.charAt(p2) != ')' && strLine.charAt(p2) != ',' && strLine.charAt(p2) != '}');
+			} while (strLine.charAt(p2) != ')' && strLine.charAt(p2) != ','
+					&& strLine.charAt(p2) != '}');
 			p1++;
 			pair = strLine.substring(p1, p2);
 			pair = pair.trim();
-			if(pair.isEmpty())
+			if (pair.isEmpty())
 				break;
-			
-			split=pair.split(" ");
-			
-			if (split.length>1)
+
+			split = pair.split(" ");
+
+			if (split.length > 1)
 				newToken.addParameter(split[1], split[0]);
-			else 
+			else
 				newToken.addParameter("", split[0]);
 
 			p1 = p2;
-			if(strLine.charAt(p2) == ')' || strLine.charAt(p2) == '}')
+			if (strLine.charAt(p2) == ')' || strLine.charAt(p2) == '}')
 				break;
-			while(strLine.charAt(p1++) == ' ');
+			while (strLine.charAt(p1++) == ' ')
+				;
 		}
 	}
 }

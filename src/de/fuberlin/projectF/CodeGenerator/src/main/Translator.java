b108dcf71ce03cb5089a24f023deffb419eb2a00
Henry <henrydettmer@gmail.com>
2012-05-27 18:17:00 +0200

    Translator erstellt jetzt seine eigene Speicherverwaltung. Erste Implementierung von Funktionsaufrufen.

--- src/de/fuberlin/projectF/CodeGenerator/src/main/Translator.java
+++ src/de/fuberlin/projectF/CodeGenerator/src/main/Translator.java
@@ -1,146 +1,207 @@
 package main;
 
-import java.util.List;
+import java.util.ArrayList;
 
-import main.model.Address;
+import main.model.Token.Parameter;
+
 import main.model.RegisterAddress;
-import main.model.RegisterInformation;
-import main.model.StackAddress;
 import main.model.Token;
 import main.model.Variable;
 
 public class Translator {
 	private StringBuffer sectionData;
 	private StringBuffer sectionText;
+	private MemoryManager mem;
+	ArrayList<Token> code;
 
-	private VariableTableContainer vars;
-
-	// private Registerverwaltung regs;
-
-	public Translator(VariableTableContainer varCon) {
-		/* Variablen/Registerverwaltung erstellen (oder werden die übergeben?) */
-		vars = varCon;
-
+	public Translator() {
+		mem = new MemoryManager();
 		sectionData = new StringBuffer().append(".section .data\n");
 		sectionText = new StringBuffer().append(".section .text\n");
-		sectionText.append(".global main\n");
 	}
 
-	public void translate(Token t) {
-		Variable target;
-		Variable op1;
-		Variable op2;
-		List<Address> addr;
-		List<Address> reg;
-		String ziel = "undefined";
-		String quelle = "undefined";
+	public void translate(ArrayList<Token> code) {
+		this.code = code;
+		for (Token tok : code) {
+			String op1, op2;
+			RegisterAddress res;
+			
+			switch (tok.getType()) {
+			case Definition:
+				// Neuen Variablenkontext anlegen
+				String name = tok.getTarget().substring(1);
+				mem.newContext(name);
+				mem.setContext(name);
+				// Deklaration
+				sectionText.append(".type ").append(name)
+						.append(", @function\n").append(name).append(":\n");
+				// Parameterbehandlung
+				if (tok.getParameterCount() > 0) {
+					for (int i = 0; i < tok.getParameterCount(); i++) {
+						Parameter p = tok.getParameter(i);
+						mem.addStackVar(p.getOperand(), p.getType(), 8 + i
+								* getSize(p.getType()));
+					}
+				}
+				sectionText.append("\tenter $0, $0\n");
+				break;
 
-		switch (t.getType()) {
-		case Definition:
-			// an dieser Stelle wechseln wir auch den Variablenkontext?
-			String name = t.getTarget().substring(1);
-			sectionText.append("type ").append(name).append(", @function\n")
-					.append(name).append(":\n");
-			sectionText.append("\tenter $0, $0\n"); // Durch spätere Optimierung
-													// oder durch lookahead auf
-													// die nächsten Token könnte
-													// hier bereits
-													// Stackspeicher reserviert
-													// werden,
-			/* TODO: Variablenkontext wechseln, Sonderbehandlung main */
-			break;
+			case Return:
+				// Kein Rückgabewert
+				if (tok.getTypeOp1().equals("void"))
+					break;
+				// Variable zurückgeben
+				res = mem.getFreeRegister();
+				mem.setReturnRegister(res);
+				if (tok.getOp1().startsWith("%")) {
+					
+					movl(mem.getAddress(tok.getOp1()), res.getFullName(),
+							"Return value");	
+				}
+				// Fester Wert
+				else
+					movl("$" + tok.getOp1(), res.getFullName(), "Return Value");
+				break;
 
-		case DefinitionEnd:
-			// hier sollten wir den Varibalenkontext zur�ckgeben
-			sectionText.append("\tleave\n");
-			sectionText.append("\tret\n");
-			break;
+			case DefinitionEnd:
+				sectionText.append("\tleave\n");
+				sectionText.append("\tret\n\n");
+				break;
 
-		case Label:
-			sectionText.append("_" + t.getTarget() + ":\n");
-			break;
+			case Call:
+				String function = tok.getOp1().substring(1);
+				// Parameter auf den Stack legen
+				if (tok.getParameterCount() > 0) {
+					for (int i = 0; i < tok.getParameterCount(); i++) {
+						Parameter p = tok.getParameter(i);
+						String operand;
+						if (p.getOperand().startsWith("%"))
+							operand = mem.getAddress(p.getOperand());
+						else
+							operand = "$" + p.getOperand();
+						pushl(operand, "Parameter " + p.getOperand());
+					}
+				}
+				// Funktionsaufruf
+				call(function);
 
-		case Allocation:
-			target = vars.getVariable(t.getTarget());
+				// Rückgabe speichern
+				mem.newRegVar(tok.getTarget(), tok.getTypeTarget(),
+						mem.getReturnRegister(function));
+				// Parameter löschen
+				for (int i = 0; i < tok.getParameterCount(); i++) {
+					Parameter p = tok.getParameter(i);
+					addl("$" + String.valueOf(getSize(p.getType())), "%esp",
+							"Dismiss Parameter");
+				}
+				break;
 
-			String size = new String("$" + target.size());
+			case Allocation:
+				// Neue Variable anlegen
+				Variable newVar = mem.newStackVar(tok.getTarget(),
+						tok.getTypeTarget());
+				// Stackpointer verschieben
+				subl("$" + String.valueOf(newVar.getSize()), "%esp",
+						"Allocation " + tok.getTarget());
+				break;
 
-			sectionText.append("\tsubl " + size + ", %esp \t\t#"
-					+ t.getTarget() + "\t#allocation\n");
-			break;
-		case Assignment:
+			case Assignment:
+				String target = mem.getAddress(tok.getTarget());
+				String source;
+				// Zuweisung Variable
+				if (mem.onStack(tok.getOp1()) && mem.onStack(tok.getTarget())) {
+					RegisterAddress tmp = mem.getFreeRegister();
+					movl(mem.getAddress(tok.getOp1()), tmp.getFullName(),
+							"Copy assignment");
+					movl(tmp.getFullName(), target,
+							tok.getTarget() + tok.getOp1());
+					mem.freeRegister(tmp);
+				} else if (tok.getOp1().startsWith("%")) {
+					source = mem.getAddress(tok.getOp1());
+					movl(source, target, "Assignment " + tok.getTarget());
+				} else
+					movl("$" + tok.getOp1(), target,
+							"Assignment " + tok.getTarget());
 
-			target = vars.getVariable(t.getTarget());
-			addr = vars.getAddresses(target);
-			for (Address a : addr) {
-				ziel = new String("-" + ((StackAddress) a).getAddress()
-						+ "(%ebp)");
+				break;
+
+			case Load:
+				mem.newVirtualVar(tok.getTarget(), tok.getOp1());
+				break;
+
+			case Addition:
+				res = mem.getFreeRegister();
+				
+				if (tok.getOp1().startsWith("%"))
+					op1 = mem.getAddress(tok.getOp1());
+				else
+					op1 = "$" + tok.getOp1();
+				if (tok.getOp2().startsWith("%"))
+					op2 = mem.getAddress(tok.getOp2());
+				else
+					op2 = "$" + tok.getOp2();
+
+				movl(op1, res.getFullName(), "Addition");
+				addl(op2, res.getFullName(),
+						tok.getOp1() + " + " + tok.getOp2());
+
+				mem.newRegVar(tok.getTarget(), tok.getTypeTarget(), res);
+
+				break;
+				
+			case Subtraction:
+				res = mem.getFreeRegister();
+
+				if (tok.getOp1().startsWith("%"))
+					op1 = mem.getAddress(tok.getOp1());
+				else
+					op1 = "$" + tok.getOp1();
+				if (tok.getOp2().startsWith("%"))
+					op2 = mem.getAddress(tok.getOp2());
+				else
+					op2 = "$" + tok.getOp2();
+
+				movl(op1, res.getFullName(), "Subtraction");
+				subl(op2, res.getFullName(),
+						tok.getOp1() + " + " + tok.getOp2());
+
+				mem.newRegVar(tok.getTarget(), tok.getTypeTarget(), res);
+
+				break;
+
+			default:
 				break;
 			}
-
-			if (t.getOp1().charAt(0) == '%') {
-				op1 = vars.getVariable(t.getOp1());
-				Address addr1 = getRegister(op1);
-				quelle = addr1.getName();
-			}
-			else
-				quelle = new String("$" + t.getOp1());
-			
-			System.out.println(target.type());
-			System.out.println(ziel);
-
-			sectionText.append("\tmovl " + quelle + ", " + ziel + "\t#assignement\n");
-
-			break;
-
-		case Load:
-			
-			op1 = vars.getVariable(t.getOp1());
-			Address address = getRegister(op1);
-			if (op1 != null)
-				System.out.println("defined");
-			else
-				System.out.println("undefined");
-
-			quelle = "-" + vars.getHomeAddress(op1).getName() + "(%ebp)";
-			ziel = address.getName();
-			
-			sectionText.append("\tmovl " + quelle + ", " + ziel
-					+ " \t\t#load\n");
-			break;
-
-		case Addition:
-			op1 = vars.getVariable(t.getOp1());
-			op2 = vars.getVariable(t.getOp2());
-			
-			System.out.println(op1);
-			System.out.println(t.getOp1());
-			RegisterAddress addr1 = (RegisterAddress)getRegister(op1);
-			RegisterAddress addr2 = (RegisterAddress)getRegister(op2);
-			sectionText.append("\taddl " + addr1.getName() + ", " + addr2.getName() + " \t\t#addition\n");
-		    //TODO : varAdminstration abdaten
-			Variable tmpVar = new Variable(t.getTarget(),t.getTypeTarget(),null);
-			vars.addVariable(tmpVar);
-			vars.getVariableTable(null).loadVarInReg(tmpVar, addr1);
-		default:
-			break;
 		}
+
+		sectionText.append(".globl _start\n_start:\n\tcall main\n" + "\tpushl "
+				+ mem.getReturnRegister("main").getFullName()
+				+ "\n\tcall exit\n\n");
 	}
 
-	private Address getRegister(Variable var) {
-		List<Address> addrList = vars.getAddresses(var);
-		Address addr = null;
-		for (Address a : addrList) {
-			if (a instanceof RegisterAddress) {
-				addr = a;
-				break;
-			}
-		}
-		if (addr == null) {
-			System.out.println("Warning : no RegisterAddress found");
-			addr = addrList.get(0);
-		}
-		return addr;
+	private void call(String name) {
+		sectionText.append("\tcall ").append(name).append("\t#Call ")
+				.append(name).append("\n");
+	}
+
+	private void pushl(String operand, String comment) {
+		sectionText.append("\tpushl ").append(operand).append("\t#")
+				.append(comment).append("\n");
+	}
+
+	private void movl(String source, String target, String comment) {
+		sectionText.append("\tmovl ").append(source).append(", ")
+				.append(target).append("\t#").append(comment).append("\n");
+	}
+
+	private void subl(String source, String target, String comment) {
+		sectionText.append("\tsubl ").append(source).append(", ")
+				.append(target).append("\t#").append(comment).append("\n");
+	}
+
+	private void addl(String source, String target, String comment) {
+		sectionText.append("\taddl ").append(source).append(", ")
+				.append(target).append("\t#").append(comment).append("\n");
 	}
 
 	public String getCode() {
@@ -156,6 +217,13 @@ public class Translator {
 
 	public void addCode(String code) {
 		sectionText.append(code + "\t#added extra\n");
+	}
+
+	private static int getSize(String type) {
+		if (type.equals("i32"))
+			return 4;
+		else
+			return 4;
 	}
 
 }

ca3e8c4531bcd5e9e81043b6c931c8f714a97815
Henry <henrydettmer@gmail.com>
2012-05-27 18:14:51 +0200

    GUI von Peter

--- src/de/fuberlin/projectF/CodeGenerator/src/main/GUI.java
+++ src/de/fuberlin/projectF/CodeGenerator/src/main/GUI.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.swing.JFrame;
@@ -9,34 +10,33 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import main.model.Token;
 
 public class GUI {
-	
+
 	Vector<String> tableColl;
 	Vector<Vector<String>> tableRow;
 	JTextArea codeArea;
-	
+
 	public GUI() {
 		JFrame window = new JFrame("CodeGenerator Debugger");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setLayout(new GridLayout(0,2));
-		
+		window.setLayout(new GridLayout(0, 2));
+
 		window.getContentPane().add(createTokenStreamTable());
 		window.getContentPane().add(createCodeTextField());
-		
+
 		window.pack();
 		window.setVisible(true);
 	}
-	
+
 	private JPanel createCodeTextField() {
 		JPanel codePanel = new JPanel();
-		
-		codeArea = new JTextArea(21,40);
-		
+
+		codeArea = new JTextArea(21, 40);
+
 		JScrollPane scrollPane = new JScrollPane(codeArea);
 		codePanel.add(scrollPane);
 		return codePanel;
@@ -44,7 +44,7 @@ public class GUI {
 
 	private JPanel createTokenStreamTable() {
 		JPanel tablePanel = new JPanel();
-		
+
 		tableColl = new Vector<String>();
 		tableRow = new Vector<Vector<String>>();
 		tableColl.add("#");
@@ -56,35 +56,39 @@ public class GUI {
 		tableColl.add("op2");
 		tableColl.add("typeOp2");
 		tableColl.add("parameter");
-		
-		DefaultTableModel tableModle = new DefaultTableModel(tableRow,tableColl);
+
+		DefaultTableModel tableModle = new DefaultTableModel(tableRow,
+				tableColl);
 		JTable table = new JTable(tableModle);
-		table.setPreferredScrollableViewportSize(new Dimension(450, 300));
+		table.setPreferredScrollableViewportSize(new Dimension(500, 300));
 		table.setFillsViewportHeight(true);
-		
+
 		JScrollPane scrollPane = new JScrollPane(table);
 		tablePanel.add(scrollPane);
 		return tablePanel;
 	}
 
-	public void updateTokenStream(Token tok) {
-		
-		Vector<String> data = new Vector<String>();
-		data.addElement(new String("" + tableRow.size()));
-		data.addElement(tok.getType().toString());
-		data.addElement(tok.getTarget());
-		data.addElement(tok.getTypeTarget());
-		data.addElement(tok.getOp1());
-		data.addElement(tok.getTypeOp1());
-		data.addElement(tok.getOp2());
-		data.addElement(tok.getTypeOp2());
-		data.addElement(new String("" + tok.getParameterCount()));
-		
-		tableRow.addElement(data);
+	public void updateTokenStreamTable(ArrayList<Token> code) {
+		for (Token t : code) {
+			Vector<String> data = new Vector<String>();
+			data.addElement(new String("" + tableRow.size()));
+			data.addElement(t.getType().toString());
+			data.addElement(t.getTarget());
+			data.addElement(t.getTypeTarget());
+			data.addElement(t.getOp1());
+			data.addElement(t.getTypeOp1());
+			data.addElement(t.getOp2());
+			data.addElement(t.getTypeOp2());
+			data.addElement(new String("" + t.getParameterCount()));
+			tableRow.addElement(data);
+		}
 	}
-	
+
 	public void updateCodeArea(String code) {
 		codeArea.setText(code);
 	}
-}
 
+	public void appendCodeArea(String text) {
+		codeArea.append(text);
+	}
+}

1290b2e041fc1f90fd9f4ad89c6f87d4554dec85
Daniel Rotar <daniel.rotar@fu-berlin.de>
2012-05-28 20:57:47 +0200

    Just added another assert to the TokenizerTest

--- bii/lexergenerator/src/test/java/tokenmatcher/TokenizerTest.java
+++ bii/lexergenerator/src/test/java/tokenmatcher/TokenizerTest.java
@@ -56,11 +56,7 @@ public class TokenizerTest {
 			i++;
 		}
 		
-		//TODO while (true) durch etwas wie while canGetNextToken oder while not EndOffile...
-//		if (i+1 != tokensToFind.length)
-//		{
-//			throw new Exception("Es wurden nicht alle tokens gefunden!");
-//		}
+		Assert.assertEquals(i, tokensToFind.length);
 	}
 	
 	/**

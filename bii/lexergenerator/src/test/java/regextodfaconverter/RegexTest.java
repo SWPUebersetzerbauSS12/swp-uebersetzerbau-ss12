c11f1e00edd399689bb3d1644a6f43d0384cd34e
Daniel Rotar <daniel.rotar@fu-berlin.de>
2012-05-29 21:51:06 +0200

    Added test from Wojciech

--- bii/lexergenerator/src/test/java/regextodfaconverter/RegexTest.java
+++ bii/lexergenerator/src/test/java/regextodfaconverter/RegexTest.java
@@ -1,9 +1,9 @@
 package regextodfaconverter;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import regextodfaconverter.Regex;
+import org.junit.Test;
 
 /**
  * Test-Klasse für die Regex-Klasse.
@@ -21,11 +21,88 @@ public class RegexTest {
 		String resultRegex;
 		
 		resultRegex = Regex.reduceAndBracketRegex("abc");
-		Assert.assertEquals("(((a)(b))(c))", resultRegex);
+		assertEquals("(((a)(b))(c))", resultRegex);
 		
 		resultRegex = Regex.reduceAndBracketRegex("a{3}");
-		Assert.assertEquals("(((a)(a))(a))", resultRegex);
+		assertEquals("(((a)(a))(a))", resultRegex);
 		
-		//TODO @Wojciech: weitere Testfälle implementieren.
+		
+		resultRegex = Regex.reduceAndBracketRegex("[a-d]");
+		assertEquals("((((a)|(b))|(c))|(d))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("[a-c-]");
+		assertEquals("((((\\-)|(a))|(b))|(c))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("[-a-c]");
+		assertEquals("((((\\-)|(a))|(b))|(c))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("[a-a]");
+		assertEquals("(a)", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("[a-bc-d]");
+		assertEquals("((((a)|(b))|(c))|(d))", resultRegex);
+		
+		try
+		{
+			resultRegex = Regex.reduceAndBracketRegex("[b-a]");
+			fail("Exception should have been thrown");
+		}
+		catch (Exception e)
+		{			
+		}
+		
+		resultRegex = Regex.reduceAndBracketRegex("ba*");
+		assertEquals("((b)((a)*))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("a*");
+		assertEquals("((a)*)", resultRegex);
+
+		resultRegex = Regex.reduceAndBracketRegex("ab+");
+		assertEquals("((a)((b)((b)*)))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("a+");
+		assertEquals("((a)((a)*))", resultRegex);
+		
+		try
+		{
+			resultRegex = Regex.reduceAndBracketRegex("a$");
+			fail("Exception should have been thrown");
+		}
+		catch (Exception e)
+		{			
+		}
+		
+		resultRegex = Regex.reduceAndBracketRegex("a\\$");
+		assertEquals("((a)(\\$))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("(a|b)?");
+		assertEquals("(()|((a)|(b)))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("a{1,1}");
+		assertEquals("(a)", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("(ab){1,2}");
+		assertEquals("(((a)(b))|(((a)(b))((a)(b))))", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("(ab){0,2}");
+		assertEquals("((()|((a)(b)))|(((a)(b))((a)(b))))", resultRegex);
+
+		try
+		{
+			resultRegex = Regex.reduceAndBracketRegex("a{1,0}");
+			fail("Exception should have been thrown");
+		}
+		catch (Exception e)
+		{			
+		}
+
+		resultRegex = Regex.reduceAndBracketRegex("[.]");
+		assertEquals("(\\.)", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("[}]");
+		assertEquals("(\\})", resultRegex);
+		
+		resultRegex = Regex.reduceAndBracketRegex("[]]");
+		assertEquals("(\\])", resultRegex);		
 	}
 }

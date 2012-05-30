cc6cd6fa261a68e99a2a171fd83293fe0b73c79c
Michael Wittig <michael.wittig@fu-berlin.de>
2012-05-30 17:18:20 +0200

    added missing @Getter in BasicType

--- src/de/fuberlin/projecta/src/main/java/analysis/ast/nodes/BasicType.java
+++ src/de/fuberlin/projecta/src/main/java/analysis/ast/nodes/BasicType.java
@@ -1,12 +1,14 @@
 package analysis.ast.nodes;
 
-import analysis.SymbolTableStack;
 import lexer.TokenType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import analysis.SymbolTableStack;
 
 @AllArgsConstructor
 public class BasicType extends Type {
 		
+	@Getter
 	TokenType type;
 	
 	public void run(SymbolTableStack tables) {

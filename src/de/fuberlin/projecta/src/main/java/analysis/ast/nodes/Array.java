1122c818451f10ea207997d2adf417de36b37e43
Michael Wittig <michael.wittig@fu-berlin.de>
2012-06-04 10:13:54 +0200

    deleted unnaccessary equals method in Array

--- src/de/fuberlin/projecta/src/main/java/analysis/ast/nodes/Array.java
+++ src/de/fuberlin/projecta/src/main/java/analysis/ast/nodes/Array.java
@@ -12,12 +12,4 @@ public class Array extends Type {
 	public void buildSymbolTable(SymbolTableStack tables) {
 
 	}
-	
-	@Override
-	public boolean equals(Object object){
-		if(object instanceof Array){
-//			for()
-		}
-		return false;
-	}
 }

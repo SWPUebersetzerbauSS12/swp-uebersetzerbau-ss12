918312422963767fc4adf078b99f9c7189e4b96e
Bjoern Karger <bjoern.karger@googlemail.com>
2012-06-06 16:45:42 +0200

    Local Common Expressions added.

--- src/de/fuberlin/optimierung/ILLVM_Node.java
+++
@@ -1,9 +0,0 @@
-package de.fuberlin.optimierung;
-
-import java.util.LinkedList;
-
-public interface ILLVM_Node {
-	public LinkedList<ILLVM_Node> getChildren();
-	public boolean addParent(ILLVM_Node parent);
-	public ILLVM_Command getCommand();
-}

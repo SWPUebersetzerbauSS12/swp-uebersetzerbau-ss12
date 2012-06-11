c330995789573d38750101dd6db4eb659f461de9
mxm <max.michels@gmail.com>
2012-06-11 12:36:38 +0200

    symbolic commit for code generation ;)

---
+++ src/de/fuberlin/projecta/src/main/java/codegen/CodeFragment.java
@@ -0,0 +1,16 @@
+package codegen;
+
+public class CodeFragment {
+	
+	/**
+	 * For managing the code fragments
+	 */
+	
+	private String code;
+	private int line;
+	
+	public CodeFragment(){
+		
+	}
+
+}

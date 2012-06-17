d9625fa73d11654ed24b33cbce83947bfb6ff559
mxm <max.michels@gmail.com>
2012-06-18 00:06:22 +0200

    corrected code representation of a StringLiteral

--- src/de/fuberlin/projecta/analysis/ast/nodes/StringLiteral.java
+++ src/de/fuberlin/projecta/analysis/ast/nodes/StringLiteral.java
@@ -3,13 +3,13 @@ package de.fuberlin.projecta.analysis.ast.nodes;
 import de.fuberlin.projecta.analysis.SymbolTableStack;
 
 public class StringLiteral extends Statement {
-	
+
 	private String value;
-	
+
 	public StringLiteral(String value) {
 		this.value = value;
 	}
-	
+
 	public void buildSymbolTable(SymbolTableStack tables) {
 
 	}
@@ -19,9 +19,20 @@ public class StringLiteral extends Statement {
 		return true;
 	}
 
+	/* How to create a string in llvm and output it (it's easy once you get it..)
+  %str = alloca [5 x i8]
+  store [5 x i8] c"test\00", [5 x i8]* %str
+  %1 = getelementptr [5 x i8]* %str, i8 0, i8 0
+  %2 = tail call i32 (i8*)* @puts(i8* %1)
+	 */
+	
 	@Override
 	public String genCode() {
-		return this.value + "\\00";
+		return "c\"" + this.value  + "\\00" + "\"";
+	}
+
+	public int getLength(){
+		return value.length();
 	}
 	
 	public String getValue() {

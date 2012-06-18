71a531ea8f7f7aad5fe5051ee424d8b38bfa4b0e
mxm <max.michels@gmail.com>
2012-06-19 00:41:18 +0200

    omg STRINGS!!!!!!1111elfeins

--- src/de/fuberlin/projecta/analysis/ast/nodes/Program.java
+++ src/de/fuberlin/projecta/analysis/ast/nodes/Program.java
@@ -41,7 +41,7 @@ public class Program extends AbstractSyntaxTree {
 	
 	/*
 	 * GenCode already implemented by AbstractSyntaxTree
-	 * - i don't care
+	 * - i don't care, we need some declarations
 	 */
 	public String genCode(){
 		//we use puts to print to screen
--- src/de/fuberlin/projecta/analysis/ast/nodes/StringLiteral.java
+++ src/de/fuberlin/projecta/analysis/ast/nodes/StringLiteral.java
@@ -13,19 +13,6 @@ public class StringLiteral extends Statement {
 	public boolean checkSemantics() {
 		return true;
 	}
-
-	/* How to create a string in llvm and output it (it's easy once you get it..)
-Using i8 arrays:
-  %str = alloca [5 x i8]	; we created an 5 byte string
-  store [5 x i8] c"test\00", [5 x i8]* %str
-  %1 = getelementptr [5 x i8]* %str, i8 0, i8 0
-  tail call i32 (i8*)* @puts(i8* %1)
-Using i8 pointer:
-  %str2 = alloca i8, i8 9   ;we created an 9 byte string
-  %1 = bitcast i8* %str2 to [9 x i8]*
-  store [9 x i8] c"test1234\00", [9 x i8]* %1
-  tail call i32 (i8*)* @puts(i8* %1)
-	 */
 	
 	@Override
 	public String genCode() {

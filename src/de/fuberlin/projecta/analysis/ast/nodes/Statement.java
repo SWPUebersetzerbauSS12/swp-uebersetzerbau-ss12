8305e299dfbcd444ee97d89194c58e5abaf4c8d7
Michael Wittig <michael.wittig@fu-berlin.de>
2012-06-24 18:49:55 +0200

    removed getHighestBlock to AbstractSyntaxTree

--- src/de/fuberlin/projecta/analysis/ast/nodes/Statement.java
+++ src/de/fuberlin/projecta/analysis/ast/nodes/Statement.java
@@ -1,22 +1,8 @@
 package de.fuberlin.projecta.analysis.ast.nodes;
 
-import de.fuberlin.commons.parser.ISyntaxTree;
 
 
 
 public abstract class Statement extends AbstractSyntaxTree {
 	
-	public Block getHighestBlock(){
-		Block block = null;
-		if(getParent() != null){
-			ISyntaxTree parent = getParent();
-			while(parent != null){
-				if(parent instanceof Block){
-					block = (Block) parent;					
-				}
-				parent = parent.getParent();
-			}
-		}		
-		return block;
-	}
 }

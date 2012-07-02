0452f0186339de7e70be4a4568587e6df406b445
Kevin Funk <kevin.funk@fu-berlin.de>
2012-07-02 13:58:20 +0200

    Add MainTest

---
+++ tests/MainTest.java
@@ -0,0 +1,78 @@
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import de.fuberlin.Main;
+
+/**
+ * Not a real unit test at the moment,
+ * but useful for quickly testing parameters to main()
+ */
+public class MainTest {
+
+	static String readFile(String file) throws FileNotFoundException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String content = "";
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				content += line + '\n';
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+		return content;
+	}
+
+	// Helper
+	static String createEmptyFile() {
+		File file;
+		try {
+			file = File.createTempFile("test", ".tmp");
+		} catch (IOException e) {
+			System.err.println("Failed to create temporary file: " + e);
+			return "";
+		}
+		return file.getAbsolutePath();
+	}
+
+	// Helper
+	static String createFile(String content) {
+		File file = null;
+		try {
+			file = File.createTempFile("test", ".ll");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.write(content);
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("Failed to create temporary file: " + e);
+			return "";
+		}
+		assert(file != null);
+		return file.getAbsolutePath();
+	}
+
+	@Test
+	public void testAllProjecta() throws Exception {
+		final String inputFile = createFile("def int main() { return 0; }");
+		final String outputFile = createEmptyFile();
+		String[] args = {"-f", inputFile, "-o", outputFile};
+		Main.main(args);
+
+		// check output file
+		String output = readFile(outputFile);
+		System.out.println(outputFile);
+		assertFalse(output.isEmpty());
+		System.out.println(output);
+	}
+
+}

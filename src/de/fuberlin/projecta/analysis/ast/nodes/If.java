92747d2b3117f22d2180ebca3c07a069d0b46121
Michael Wittig <michael.wittig@fu-berlin.de>
2012-07-16 11:01:06 +0200

    implmented if(id) in if

--- src/de/fuberlin/projecta/analysis/ast/nodes/If.java
+++ src/de/fuberlin/projecta/analysis/ast/nodes/If.java
@@ -25,9 +25,18 @@ public class If extends Statement {
 			this.setBeginLabel(label);
 			ret += "br label %" + label + "\n\n";
 			ret += "; <label> %" + label + "\n";
-			ret += ((AbstractSyntaxTree) getChild(0)).genCode();
-
-			ret += LLVM.genBranch(this, ((AbstractSyntaxTree) getChild(1)),null, not, false);
+			
+			if(!(getChild(0) instanceof Id)){
+				ret += ((AbstractSyntaxTree) getChild(0)).genCode();
+				ret += LLVM.genBranch(this, ((AbstractSyntaxTree) getChild(1)),null, not, false);
+			} else {
+				Id id = (Id) getChild(0);
+				int tmp1 = block.getNewVar();
+				ret += "%" + tmp1 + " = load i1* %" + id.getValue() + "\n";
+				int tmp2 = block.getNewVar();
+				ret += "%" + tmp2 + " = icmp ne i1 %" + tmp1 + ", 0\n";
+				ret += ret += LLVM.genBranch(this, ((AbstractSyntaxTree) getChild(1)),null, not, false);
+			}			
 		}
 		return ret;
 	}

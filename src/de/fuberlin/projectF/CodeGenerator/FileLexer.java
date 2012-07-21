9def9617ace5c37ad7207d64674bab854303d026
Peter Hirschfeld <burnde@gmx.de>
2012-07-21 21:57:27 +0200

    add comments to Lexer.java

--- src/de/fuberlin/projectF/CodeGenerator/FileLexer.java
+++ src/de/fuberlin/projectF/CodeGenerator/FileLexer.java
@@ -5,21 +5,7 @@ import java.io.*;
 import de.fuberlin.projectF.CodeGenerator.model.Token;
 import de.fuberlin.projectF.CodeGenerator.model.TokenType;
 
-/**
- *         Dies ist ein Lexer für einen LLVM Code. Grobe Funktionsweise: Der
- *         LLVM Code wird zeilenweise durchgeparst und es wird pro geparste
- *         Zeile ein Token erzeugt und zurückgegeben.
- * 
- *         Es gibt 3 wesentliche Funktionen: open: öffnet die LLVM-Code Datei
- *         close: schließt die LLVM-Code Datei getNextToken: liesst eine Zeile
- *         der Eingabe Datei und parst diese dann auf relevante Informationen
- *         Diese Informationen werden dann in einem Token gespeichert. Der Token
- *         wird dann zurückgegeben.
- * 
- *         Ist aber noch nicht fertig!!!!!!
- * 
- */
-
+//Diese Funktion 
 public class FileLexer extends Lexer{
 
 	FileInputStream fstream;
@@ -36,17 +22,11 @@ public class FileLexer extends Lexer{
 	// öffnen der Datei
 	public int open(File file) throws FileNotFoundException{
 		debug.print("Open input file " + file.toString() + " ...");
-		//try {
+		
 			fstream = new FileInputStream(file);
 			in = new DataInputStream(fstream);
 			br = new BufferedReader(new InputStreamReader(in));
 
-		//} catch (FileNotFoundException e) {
-		//	debug.print("\tError while try to open " + file.toString());
-		//	System.err.println("Could't find input file " + file.toString());
-		//	e.printStackTrace();
-		//	return -1;
-		//}
 		debug.println("...[OK]\n");
 		return 0;
 	}
--- src/de/fuberlin/projectF/CodeGenerator/StringLexer.java
+++ src/de/fuberlin/projectF/CodeGenerator/StringLexer.java
@@ -5,21 +5,8 @@ import java.io.*;
 import de.fuberlin.projectF.CodeGenerator.model.Token;
 import de.fuberlin.projectF.CodeGenerator.model.TokenType;
 
-/**
- *         Dies ist ein Lexer für einen LLVM Code. Grobe Funktionsweise: Der
- *         LLVM Code wird zeilenweise durchgeparst und es wird pro geparste
- *         Zeile ein Token erzeugt und zurückgegeben.
- * 
- *         Es gibt 3 wesentliche Funktionen: open: öffnet die LLVM-Code Datei
- *         close: schließt die LLVM-Code Datei getNextToken: liesst eine Zeile
- *         der Eingabe Datei und parst diese dann auf relevante Informationen
- *         Diese Informationen werden dann in einem Token gespeichert. Der Token
- *         wird dann zurückgegeben.
- * 
- *         Ist aber noch nicht fertig!!!!!!
- * 
- */
-
+//Die Klasse StringLexer ist zuständig für das Parsen eines Strings in dem der
+//zu übersetzende LLVM Code enthalten ist. Sie erbt alle Funktionen vom Lexer
 public class StringLexer extends Lexer{
 
 	FileInputStream fstream;
@@ -39,6 +26,7 @@ public class StringLexer extends Lexer{
 		return 0;
 	}
 
+	//geht durch alle LLVM Zeilen und erzeugt die Token
 	public Token getNextToken() {
 		Token token;
 		while (cursor < code.length) {
